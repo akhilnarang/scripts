1f63704cfdcca68d9e2d730ed494b1fca0ec495b
Akhil Narang <randomlybored@openmailbox.org>
2015-10-01 18:06:30 +0530

    fix installation

--- server-build-environment-setup.sh
+++ server-build-environment-setup.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
 squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
 schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
-lib32readline-gplv2-dev gcc-multilib liblz4-* pngquant jenkins ncurses-dev texinfo gcc gperf patch libtool\
+lib32readline-gplv2-dev gcc-multilib liblz4-* pngquant jenkins ncurses-dev texinfo gcc gperf patch libtool \
 automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static libgcc1:i386 bc libcloog-isl-dev \
 libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* liblzma*
 clear
