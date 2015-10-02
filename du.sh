9101fc17d81e5e4531b67b103a575c0f5059529a
Akhil Narang <akhilnarang.1999@gmail.com>
2015-10-02 22:01:31 +0200

    fix jenkins showing success on failed build :P
    @anik1199 please do for all when free
    
    Signed-off-by: Blazing Phoenix <blazingphoenixdevs@gmail.com>

--- du.sh
+++ du.sh
@@ -70,21 +70,7 @@ fi
 echo -e "Lunching $DEVICE"
 lunch du_$DEVICE-userdebug
 
-### Build and log output to a log file
+### Build
 echo -e "Starting DirtyUnicorns build in 5 seconds"
 sleep 5
-make -j8 bacon  2>&1 | tee DU_$DEVICE-$(date +%Y%m%d).log
-
-### Copying of zip and build log
-
-if [ ! -e "$UPLOAD_DIR" ];
-then
-echo -e "Dir to copy zip not found, creating";
-mkdir -p $UPLOAD_DIR
-fi
-echo -e "Copying zip, build log, zip md5sum";
-cp out/target/product/$DEVICE/DU*.zip $UPLOAD_DIR/
-cp DU_$DEVICE-*.log $UPLOAD_DIR/
-cp out/target/product/$DEVICE/DU*.zip.md5sum $UPLOAD_DIR/
-echo -e "All required outputs copied to $UPLOAD_DIR please use upload_du script to upload :)"
-echo -e "Have a nice day :), enjoy the power of BlazingPhoenix Server :D ";
+make -j8 bacon
