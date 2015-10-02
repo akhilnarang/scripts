541a5baa38b34db72b20fee311f54690dde42119
Akhil Narang <akhilnarang.1999@gmail.com>
2015-10-02 15:44:53 +0530

    scripts: add temasek scripts
    @anik1199 make your personal rom scripts like this with build user please :)
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ temasek.sh
@@ -0,0 +1,96 @@
+ #
+ # Copyright � 2015, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+
+#!/bin/bash
+user=akhilnarang
+host=$(cat /etc/hostname)
+home=/android/personal/$user/temasek
+cd $home
+export KBUILD_BUILD_USER=$user
+export KBUILD_BUILD_HOST=$host
+export LINUX_COMPILE_BY=$user
+export LINUX_COMPILE_HOST=$host
+export TARGET_UNFFICIAL_BUILD_ID="temasek"
+export WITH_LZMA_OTA=true
+CLEAN_OR_NOT=$1
+SYNC_OR_NOT=$2
+if [ ! "$3" == "" ];
+then
+export KBUILD_BUILD_USER=$3
+DEVICE=$4
+else
+DEVICE=$3
+
+export UPLOAD_DIR="/android/to-upload/temasek/$DEVICE"
+
+echo "██████╗ ██╗      █████╗ ███████╗██╗███╗   ██╗ ██████╗ ██████╗ ██╗  ██╗ ██████╗ ███████╗███╗   ██╗██╗██╗  ██╗";
+echo "██╔══██╗██║     ██╔══██╗╚══███╔╝██║████╗  ██║██╔════╝ ██╔══██╗██║  ██║██╔═══██╗██╔════╝████╗  ██║██║╚██╗██╔╝";
+echo "██████╔╝██║     ███████║  ███╔╝ ██║██╔██╗ ██║██║  ███╗██████╔╝███████║██║   ██║█████╗  ██╔██╗ ██║██║ ╚███╔╝ ";
+echo "██╔══██╗██║     ██╔══██║ ███╔╝  ██║██║╚██╗██║██║   ██║██╔═══╝ ██╔══██║██║   ██║██╔══╝  ██║╚██╗██║██║ ██╔██╗ ";
+echo "██████╔╝███████╗██║  ██║███████╗██║██║ ╚████║╚██████╔╝██║     ██║  ██║╚██████╔╝███████╗██║ ╚████║██║██╔╝ ██╗";
+echo "╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝";
+echo "                                                                                                            ";
+
+
+echo -e "Setting up build environment";
+. build/envsetup.sh
+
+### Check conditions for cleaning output directory
+if [ "$CLEAN_OR_NOT" == "1" ];
+then
+echo -e "Cleaning out directory"
+make -j8 clean > /dev/null
+echo -e "Out directory cleaned"
+elif [ "$CLEAN_OR_NOT" == "2" ];
+then
+echo -e "Making out directory dirty"
+make -j8 dirty > /dev/null
+echo -e "Deleted old zips, changelogs, build.props"
+else
+echo -e "Out directory untouched!"
+fi
+
+### Check conditions for repo sync
+if [ "$SYNC_OR_NOT" == "1" ];
+then
+echo -e "Running repo sync"
+repo sync -cfj8 --force-sync --no-clone-bundle
+echo -e "Repo sync complete"
+else
+echo -e "Not syncing!"
+fi
+
+### Lunching device
+echo -e "Lunching $DEVICE"
+lunch cm_$DEVICE-userdebug
+
+### Build and log output to a log file
+echo -e "Starting Temasek's unofficial cm-12.1 build in 5 seconds"
+sleep 5
+make -j8 bacon  2>&1 | tee temasek_$DEVICE-$(date "+%Y%m%d").log
+
+### Copying of zip and build log
+
+if [ ! -e "$UPLOAD_DIR" ];
+then
+echo -e "Dir to copy zip not found, creating";
+mkdir -p $UPLOAD_DIR
+fi
+echo -e "Copying zip, build log, zip md5sum";
+cp out/target/product/$DEVICE/cm*.zip $UPLOAD_DIR/
+cp temasek_$DEVICE-*.log $UPLOAD_DIR/
+cp out/target/product/$DEVICE/cm*.zip.md5sum $UPLOAD_DIR/
+echo -e "All required outputs copied to $UPLOAD_DIR please use upload_akhil_bb script to upload :)"
+echo -e "Have a nice day :), enjoy the power of BlazingPhoenix Server :D ";
