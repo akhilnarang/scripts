f2b80f786abb5121087dec165370364013387abe
Akhil Narang <akhilnarang.1999@gmail.com>
2015-10-02 12:32:51 +0200

    script: add users file
    Signed-off-by: Blazing Phoenix <blazingphoenixdevs@gmail.com>

---
+++ addusers-group.sh
@@ -0,0 +1,4 @@
+for user in ramsudharshan ujwalp15 pranav01 srisurya95 tjstevemx minidude2012 veeresh akhilnarang anik yeshwanthvshenoy jenkins
+do
+usermod -a -G common $user
+done
---
+++ addusers.sh
@@ -0,0 +1,5 @@
+for user in ramsudharshan ujwalp15 pranav01 srisurya95 tjstevemx minidude2012 veeresh
+do
+useradd -d /home/$user -m $user
+passwd $user
+done
