b265dd731b24ccd2e30eb2ffd93237709df8b427
Akhil Narang <akhilnarang.1999@gmail.com>
2015-10-03 07:22:59 +0200

    fix
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- orion.sh
+++ orion.sh
@@ -61,7 +61,7 @@ fi
 if [ "$SYNC_OR_NOT" == "1" ];
 then
 echo -e "Running repo sync"
-repo forall -c "git reset --hard"
+repo forall -vc "git reset --hard HEAD"
 repo sync -cfj8 --force-sync --no-clone-bundle
 echo -e "Repo sync complete"
 else
