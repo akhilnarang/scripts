ce77824d53a13cc75633a2aafd7970a036f96065
Akhil Narang <randomlybored@openmailbox.org>
2015-10-03 21:04:00 +0530

    Create oct.sh

---
+++ oct.sh
@@ -0,0 +1,68 @@
+ #
+ # Copyright � 2015, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+
+#!/bin/bash
+user=ujwalp15
+host=$(cat /etc/hostname)
+home=/home/$user/OctOS
+cd $home
+export KBUILD_BUILD_USER=$user
+export KBUILD_BUILD_HOST=$host
+export LINUX_COMPILE_BY=$user
+export LINUX_COMPILE_HOST=$host
+export WITH_LZMA_OTA=true
+CLEAN_OR_NOT=$1
+SYNC_OR_NOT=$2
+DEVICE=$3
+
+echo "██████╗ ██╗      █████╗ ███████╗██╗███╗   ██╗ ██████╗ ██████╗ ██╗  ██╗ ██████╗ ███████╗███╗   ██╗██╗██╗  ██╗";
+echo "██╔══██╗██║     ██╔══██╗╚══███╔╝██║████╗  ██║██╔════╝ ██╔══██╗██║  ██║██╔═══██╗██╔════╝████╗  ██║██║╚██╗██╔╝";
+echo "██████╔╝██║     ███████║  ███╔╝ ██║██╔██╗ ██║██║  ███╗██████╔╝███████║██║   ██║█████╗  ██╔██╗ ██║██║ ╚███╔╝ ";
+echo "██╔══██╗██║     ██╔══██║ ███╔╝  ██║██║╚██╗██║██║   ██║██╔═══╝ ██╔══██║██║   ██║██╔══╝  ██║╚██╗██║██║ ██╔██╗ ";
+echo "██████╔╝███████╗██║  ██║███████╗██║██║ ╚████║╚██████╔╝██║     ██║  ██║╚██████╔╝███████╗██║ ╚████║██║██╔╝ ██╗";
+echo "╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝";
+echo "                                                                                                            ";
+
+figlet Team OctOS
+echo -e "Setting up build environment";
+. build/envsetup.sh
+### Check conditions for cleaning output directory
+if [ "$CLEAN_OR_NOT" == "1" ];
+then
+echo -e "Cleaning out directory"
+make -j8 clean > /dev/null
+echo -e "Out directory cleaned"
+elif [ "$CLEAN_OR_NOT" == "2" ];
+then
+echo -e "Making out directory dirty"
+make -j8 dirty > /dev/null
+echo -e "Deleted old zips, changelogs, build.props"
+else
+echo -e "Out directory untouched!"
+fi
+### Check conditions for repo sync
+if [ "$SYNC_OR_NOT" == "1" ];
+then
+echo -e "Running repo sync"
+repo forall -vc "git reset --hard HEAD"
+repo sync -cfj8 --force-sync --no-clone-bundle
+echo -e "Repo sync complete"
+else
+echo -e "Not syncing!"
+fi
+### Build and log output to a log file
+echo -e "Starting Team OctOS build in 5 seconds"
+sleep 5
+brunch $device
