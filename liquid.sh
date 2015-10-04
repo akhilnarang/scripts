70609e3d11b0eb91cd2fa94bd93a6a22cbf8a701
Akhil Narang <akhilnarang.1999@gmail.com>
2015-10-04 18:55:51 +0200

    fix perms and add liquid
    Signed-off-by: Blazing Phoenix <blazingphoenixdevs@gmail.com>

---
+++ liquid.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /android/common/LiquidSmooth-Layers
+rm -rf .repo/local_manifests/*
+curl --create-dirs -L -o .repo/local_manifests/liquid.xml -O -L https://raw.githubusercontent.com/Anik1199/BlazingPhoenix/master/liquid.xml
+repo sync -cfj8 --force-sync --no-clone-bundle
+export TARGET_USE_O_LEVEL_3=true
+export USE_CCACHE=1
+export CCACHE_DIR=/android/.ccache
+ccache -M 500G
+. build/envsetup.sh
+if [ "$cleanOrNot" == "1" ];
+then
+make -j10 clobber
+elif [ "$cleanOrNot" == "2" ];
+then
+make -j10 dirty
+else
+figlet No Clean
+fi
+lunch liquid_$device-userdebug
+make -j10 liquid
