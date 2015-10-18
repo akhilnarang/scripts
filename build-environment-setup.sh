81d4a4a2568c8ac19ba961745a4da42d0e1a7ae1
Akhil <akhilnarang.1999@gmail.com>
2015-10-18 23:29:17 +0530

    fixup
    Signed-off-by: Akhil <akhilnarang.1999@gmail.com>

--- build-environment-setup.sh
+++ build-environment-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 clear
 echo Installing Dependencies!
-sudo apt-get update && sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib liblz4-* android-tools-adb android-tools-fastboot phablet-tools
+sudo apt-get update
+sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
+squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
+schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+lib32readline-gplv2-dev gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
+automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static libgcc1:i386 bc libcloog-isl-dev \
+libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* liblzma* phablet-tools
 clear
 echo Dependencies have been installed
 clear
@@ -18,4 +24,6 @@ git clone https://github.com/peterjc/backports.lzma /tmp/backports.lzma
 cd /tmp/backports.lzma
 sudo python2 setup.py install
 python2 test/test_lzma.py
-rm -rf /tmp/backports.lzma
+sudo rm -rf /tmp/backports.lzma
+echo LZMA compression for ROMs enabled
+echo "WITH_LZMA_OTA=true" >> ~/.bashrc
