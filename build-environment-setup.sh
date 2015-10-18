b6ef0c5d9281c14096c93e606dc629e254a77e27
Akhil <akhilnarang.1999@gmail.com>
2015-10-18 23:30:15 +0530

    I can't be the only one who checks messenger.com while ssh'd into a server :3
    Signed-off-by: Akhil <akhilnarang.1999@gmail.com>

--- build-environment-setup.sh
+++ build-environment-setup.sh
@@ -7,7 +7,8 @@ squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre
 schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
 lib32readline-gplv2-dev gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
 automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static libgcc1:i386 bc libcloog-isl-dev \
-libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* liblzma* phablet-tools
+libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
+liblzma* w3m
 clear
 echo Dependencies have been installed
 clear
