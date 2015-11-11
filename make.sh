f051b024d87ece262bb7221db84b46573119e6ef
Akhil Narang <akhilnarang.1999@gmail.com>
2015-11-12 01:01:14 +0530

    scripts: add my baws thug scripts
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ make.sh
@@ -0,0 +1,84 @@
+ #!/bin/bash
+ #
+ # Copyright � 2015, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
+ # Build Script For ThugLife Kernel
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+
+export OP_DIR=/tmp/kernel
+export ZIMAGE="$OP_DIR/arch/arm/boot/zImage"
+export ANYKERNEL=$THUGDIR/sprout/anykernel
+export THUGVERSION="ThugLife~1.3";
+export DEVICE="sprout";
+export DEFCONFIG=$DEVICE"_defconfig";
+export FINAL_ZIP="$THUGDIR/files/$DEVICE/$THUGVERSION.zip"
+export CROSS_COMPILE="$THUGDIR/thuglife-sprout-toolchain/bin/arm-cortex_a7-linux-gnueabihf-"
+
+if [ ! "$2" == "" ];
+then
+export CLEANOPTION=$2
+if [ ! "$3" == "" ];
+then
+export PUSHOPTION=$3
+fi
+fi
+if [ "$CLEANOPTION" == "clean" ] || [ "$CLEANOPTION" == "cleanbuild" ];
+then
+make clean
+make mrproper
+rm -f include/linux/autoconf.h
+rm -rf $OP_DIR
+mkdir -p $OP_DIR
+else
+export PUSHOPTION=$CLEANOPTION
+fi
+if [ -f ".config" ];
+then
+rm .config;
+fi
+if [ -f "$ZIMAGE" ];
+then
+rm -f $ZIMAGE;
+fi
+make $DEFCONFIG O=$OP_DIR
+figlet ThugLife
+START=$(date +"%s")
+make $1 O=$OP_DIR
+END=$(date +"%s")
+DIFF=$(($END - $START))
+echo -e "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.";
+if [ ! -f "$ZIMAGE" ];
+then
+echo -e "Kernel Compilation Failed!";
+echo -e "Fix The Errors!";
+echo -e "Log is at $LOGFILE";
+exit 1;
+fi
+cp -v $ZIMAGE $ANYKERNEL/tools/
+cd $ANYKERNEL
+zip -r9 $FINAL_ZIP *;
+cd ..
+if [ -f "$FINAL_ZIP" ];
+then
+echo -e "$THUGVERSION zip can be found at $FINAL_ZIP";
+if [ ! "$PUSHOPTION" == "" ];
+then
+echo -e "Pushing $FINAL_ZIP to /sdcard";
+adb kill-server
+adb start-server
+adb wait-for-device
+adb push $FINAL_ZIP /sdcard/
+fi
+else
+echo -e "Zip Creation Failed =(";
+fi
---
+++ stuff.sh
@@ -0,0 +1,37 @@
+ #!/bin/bash
+ #
+ # Copyright � 2015, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
+ # Script For ThugLife Kernel
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+
+export LOCAL_FILES=$THUGDIR/files;
+
+echo -e "Sync sprout files with SourceForge.net?";
+echo -e "Local files are :"
+ls $LOCAL_FILES/sprout/;
+echo -e "Enter 1 to upload, anything else not to";
+read ch;
+if [ "$ch" == "1" ];
+then
+rsync -av -e ssh $LOCAL_FILES/sprout/ akhilnarang@frs.sourceforge.net:/home/frs/project/thuglife/sprout/
+fi
+
+echo -e "Sync sprout files from SourceForge.net here?";
+echo -e "Enter 1 to download, anything else to not";
+read ch;
+if [ "$ch" == "1" ];
+then
+rsync -av -e ssh akhilnarang@frs.sourceforge.net:/home/frs/project/thuglife/sprout/ $LOCAL_FILES/sprout/;
+fi
+
