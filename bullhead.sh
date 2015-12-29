d12f23d24c0db51eaaf17547c47b1929bff352c8
Akhil Narang <akhilnarang.1999@gmail.com>
2015-12-29 23:31:02 +0530

    bullhead: prepare for release (2/2)
    remove outdir dir stuff, ill do manually, or build in here itself
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- bullhead.sh
+++ bullhead.sh
@@ -16,69 +16,33 @@
 #
 
 export DEVICE="bullhead";
-export OP_DIR=/tmp/$DEVICE~kernel
-export ZIMAGE="$OP_DIR/arch/arm/boot/zImage"
+export ARCH="arm64"
+export IMAGE="arch/$ARCH/boot/Image.gz-dtb"
 export ANYKERNEL=$THUGDIR/$DEVICE/anykernel
-export THUGVERSION="ThugLife~1.0~CAF-bullhead";
-export DEFCONFIG=$DEVICE"_defconfig";
+export THUGVERSION="ThugLife~1.0~bullhead";
+export DEFCONFIG="thug_defconfig";
 export FINAL_ZIP="$THUGDIR/files/$DEVICE/$THUGVERSION.zip"
-export CROSS_COMPILE="$THUGDIR/../arm-eabi-5.2-cortex-a15/bin/arm-eabi-"
-
-if [ ! -d "$OP_DIR" ];
-then
-mkdir -p $OP_DIR;
-fi
+export CROSS_COMPILE="$THUGDIR/$device-toolchain/bin/aarch64-linux-android-"
 
 cd $THUGDIR/$DEVICE
-
-make mrproper
-
-if [ ! "$2" == "" ];
-then
-export CLEANOPTION=$2
-if [ ! "$3" == "" ];
-then
-export PUSHOPTION=$3
-fi
-fi
-
-if [ "$CLEANOPTION" == "clean" ] || [ "$CLEANOPTION" == "cleanbuild" ];
-then
-make clean mrproper
-make clean mrproper O=$OP_DIR
-rm -f include/linux/autoconf.h
-else
-export PUSHOPTION=$CLEANOPTION
-fi
-
-if [ -f ".config" ];
-then
-rm .config;
-fi
-
-if [ -f "$ZIMAGE" ];
-then
-rm -f $ZIMAGE;
-fi
-
-make $DEFCONFIG O=$OP_DIR
+make $DEFCONFIG
 figlet ThugLife
 START=$(date +"%s")
-make $1 O=$OP_DIR
+make $1
 END=$(date +"%s")
 DIFF=$(($END - $START))
 echo -e "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.";
 
-if [ ! -f "$ZIMAGE" ];
+if [ ! -f "$IMAGE" ];
 then
 echo -e "Kernel Compilation Failed!";
 echo -e "Fix The Errors!";
-$THUGDIR/$DEVICE/anykernel/tools/dtbToolCM -2 -o $OP_DIR/arch/arm/boot/dt.img -s 2048 -p $OP_DIR/scripts/dtc/ $OP_DIR/arch/arm/boot/
-
 exit 1;
+else
+echo -e "Build Succesfull Enjoy Living the ThugLife!"
 fi
 
-cp -v $ZIMAGE $ANYKERNEL/tools/
+cp -v $IMAGE $ANYKERNEL/zImage
 cd $ANYKERNEL
 zip -r9 $FINAL_ZIP *;
 cd ..
