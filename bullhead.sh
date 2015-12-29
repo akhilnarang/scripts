34b09450091dc3da32b497c174e9434f4793cc25
Akhil Narang <akhilnarang.1999@gmail.com>
2015-12-29 23:33:02 +0530

    fix sleepiness
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- bullhead.sh
+++ bullhead.sh
@@ -22,7 +22,7 @@ export ANYKERNEL=$THUGDIR/$DEVICE/anykernel
 export THUGVERSION="ThugLife~1.0~bullhead";
 export DEFCONFIG="thug_defconfig";
 export FINAL_ZIP="$THUGDIR/files/$DEVICE/$THUGVERSION.zip"
-export CROSS_COMPILE="$THUGDIR/$device-toolchain/bin/aarch64-linux-android-"
+export CROSS_COMPILE="$THUGDIR/$DEVICE-toolchain/bin/aarch64-linux-android-"
 
 cd $THUGDIR/$DEVICE
 make $DEFCONFIG
