adc48cee406b05909c761ad557859afd6a160f4d
Akhil Narang <akhil.narang@protonmail.com>
2016-01-09 18:10:41 +0530

    scripts: fix some stuff

--- build-environment-setup.sh
+++ build-environment-setup.sh
@@ -8,10 +8,8 @@ schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib l
 lib32readline-gplv2-dev gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
 automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static libgcc1:i386 bc libcloog-isl-dev \
 libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
-liblzma* w3m
-clear
+liblzma* w3m phablet-tools android-tools-adb
 echo Dependencies have been installed
-clear
 echo repo has been Downloaded!
 echo Settings up USB Ports
 sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
