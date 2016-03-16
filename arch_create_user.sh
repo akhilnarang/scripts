6318b0f5fb320fd85438504be41b66d3fe1cca52
Akhil Narang <akhilnarang.1999@gmail.com>
2016-03-17 01:43:05 +0530

    scripts: fix arch linux user creator
     * Don't do anything if not run as root.
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- arch_create_user.sh
+++ arch_create_user.sh
@@ -1,6 +1,10 @@
-# username, user_ID
+if [ "$(whoami)" == "root" ];
+then
 username="$1"
 useradd "${username}"
 mkhomedir_helper "${username}"
 passwd "${username}"
 chage -d 0 "${username}"
+else
+echo -e "Please run the script with root!"
+fi
