e8f750031163b6f4e5011cb4e64d153585176d0e
Akhil Narang <akhilnarang.1999@gmail.com>
2016-04-11 16:04:39 +0530

    underp it
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- merge.sh
+++ merge.sh
@@ -22,5 +22,7 @@ usage;
 esac
 
 git reset --hard origin/master && git checkout master
-git fetch $(cat upstream)
-git merge $(cat upstream)
+mergeremote=$(cat upstream | awk '{print $1}')
+mergebranch=$(cat upstream | awk '{print $2}')
+git fetch $mergeremote $mergebranch
+git merge $mergeremote/$mergebranch
