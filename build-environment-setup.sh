d3687526d370ea066caa06156c7d72ecd7d86d6e
Harsh Shandilya <MSF-Jarvis@users.noreply.github.com>
2016-05-03 17:48:40 +0530

    Remove '-get' from apt-get (#10)
    This is shorter, plus it forces a progress bar!

--- build-environment-setup.sh
+++ build-environment-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
 echo Installing Dependencies!
-sudo apt-get update
-sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
+sudo apt update
+sudo apt -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
 squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
 schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
 gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
