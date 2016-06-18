c3e8972b7decf2669ff3720efc44ae583f8d9bb2
Gureumi <cloud9350@naver.com>
2016-06-18 14:22:25 +0900

    Remove not found packages.
    It's work from ubuntu 16.04.
    Recommand new branch.

--- build-environment-setup.sh
+++ build-environment-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright � 2015-2016, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
+# Copyright (c) 2015-2016, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
 #
 # This software is licensed under the terms of the GNU General Public
 # License version 2, as published by the Free Software Foundation, and
@@ -17,11 +17,11 @@
 clear
 echo Installing Dependencies!
 sudo apt-get update
-sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
-squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
-schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev \
+squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev pngcrush \
+schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32ncurses5-dev \
 gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
-automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static bc libcloog-isl-dev \
+automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev \
 libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
 liblzma* w3m phablet-tools android-tools-adb ccache maven ncftp
 echo Dependencies have been installed
