8d18603dbe538fd424cc1f647c55fd1b79337437
Akhil Narang <akhilnarang.1999@gmail.com>
2016-07-07 21:49:44 +0530

    scripts: htop, moar lazy
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- ubuntu1604-linuxmint18x.sh
+++ ubuntu1604-linuxmint18x.sh
@@ -16,15 +16,15 @@
 
 clear
 echo Installing Dependencies!
-sudo apt-add-repository ppa:openjdk-r/ppa
-sudo apt update
+sudo apt-add-repository ppa:openjdk-r/ppa -y
+sudo apt update -y
 sudo apt install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev \
 squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
 schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
 gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
 automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev \
 libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
-liblzma* android-tools-adb maven ncftp figlet
+liblzma* w3m android-tools-adb maven ncftp htop -y
 echo Dependencies have been installed
 echo repo has been Downloaded!
 if [ ! "$(which adb)" == "" ];
