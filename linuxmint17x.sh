296ae20189470b957d547db84cb57a7ff710e478
Akhil Narang <akhilnarang.1999@gmail.com>
2016-08-13 23:12:27 +0530

    scripts: deunify mint and ubuntu
      * Mint 17.x can use apt instead of apt-get
      * Ubuntu 14.04 can't
      * Ubuntu 16.04 dosen't have support apt-add-repository by default
      * Mint 18 does
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ linuxmint17x.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Copyright � 2015-2016, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+#
+
+clear
+echo Installing Dependencies!
+sudo apt update
+sudo apt -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
+squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
+schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
+automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static bc libcloog-isl-dev \
+libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
+liblzma* w3m android-tools-adb maven ncftp figlet
+echo Dependencies have been installed
+echo repo has been Downloaded!
+if [ ! "$(which adb)" == "" ];
+then
+echo Setting up USB Ports
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
+sudo chmod 644   /etc/udev/rules.d/51-android.rules
+sudo chown root /etc/udev/rules.d/51-android.rules
+sudo service udev restart
+adb kill-server
+sudo killall adb
+fi
+
+echo "Installing repo"
+sudo install utils/repo /usr/bin/
+echo "Installing ccache"
+sudo install utils/ccache /usr/bin/
+
---
+++ ubuntu1604.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Copyright � 2015-2016, Akhil Narang "akhilnarang" <akhilnarang.1999@gmail.com>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+#
+
+clear
+echo Installing Dependencies!
+sudo apt install -y software-properties-common
+sudo apt-add-repository ppa:openjdk-r/ppa -y
+sudo apt update -y
+sudo apt install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev \
+squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
+schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
+automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev \
+libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
+liblzma* w3m android-tools-adb maven ncftp htop -y
+echo Dependencies have been installed
+echo repo has been Downloaded!
+if [ ! "$(which adb)" == "" ];
+then
+echo Setting up USB Ports
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
+sudo chmod 644   /etc/udev/rules.d/51-android.rules
+sudo chown root /etc/udev/rules.d/51-android.rules
+sudo service udev restart
+adb kill-server
+sudo killall adb
+fi
+
+makeversion=$(make -v | head -1 | awk '{print $3}')
+if [ ! "${makeversion}" == "3.81" ];
+then
+echo "Installing make 3.81 instead of ${makeversion}"
+sudo install utils/make /usr/bin/
+fi
+echo "Installing repo"
+sudo install utils/repo /usr/bin/
+echo "Installing ccache"
+sudo install utils/ccache /usr/bin/
