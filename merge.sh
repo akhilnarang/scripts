c213ceb50cbb873924be6e17a5cb43025be8315f
Akhil Narang <akhil@akhilnarang.me>
2016-10-11 17:54:30 +0530

    Add merge script, chmod +x *
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

---
+++ merge.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+#COLORS -
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+blu=$'\e[1;34m'
+end=$'\e[0m'
+
+
+# Assumes source is in users home in a directory called "orion"
+export RROPATH="${HOME}/aosp"
+
+# Set the tag you want to merge
+export TAG="android-7.0.0_r14"
+
+do_not_merge="platform_vendor_aosp"
+# Orion manifest is setup with path first, then repo name, so the path attribute is after 2 spaces, and the name within "" in it
+for repos in $(grep 'remote="rro"' ${RROPATH}/.repo/manifests/default.xml  | awk '{print $2}' | cut -d '"' -f2)
+do
+echo -e ""
+if [[ "${do_not_merge}" =~ "${repos}" ]];
+then
+echo -e "${repos} is not to be merged";
+else
+echo "$blu Merging $repos $end"
+echo -e ""
+cd $repos;
+git fetch https://android.googlesource.com/platform/$repos -qt
+git merge $TAG
+if [ $? -ne 0 ];
+then
+echo "$repos" >> ${RROPATH}/failed
+echo "$red $repos failed :C $end"
+else
+echo "$repos" >> ${RROPATH}/success
+echo "$grn $repos succeeded $end"
+fi
+echo -e ""
+cd ${RROPATH};
+fi
+done
+
+echo -e ""
+echo -e "$red These repos failed $end"
+cat ./failed
+echo -e ""
+echo -e "$grn These succeeded $end"
+cat ./success
