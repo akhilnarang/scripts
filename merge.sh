063085385e98dc1025263eeadcc5fe99b624b1fd
Akhil Narang <akhil@akhilnarang.me>
2016-10-12 16:40:57 +0530

    merge.sh: final fixes :)
      * Special cases for the repos that aren't to be merged from AOSP
    
    Signed-off-by: Akhil Narang <akhil@akhilnarang.me>

--- merge.sh
+++ merge.sh
@@ -7,14 +7,17 @@ blu=$'\e[1;34m'
 end=$'\e[0m'
 
 
-# Assumes source is in users home in a directory called "orion"
-export RROPATH="${HOME}/aosp"
+# Assumes source is in users home in a directory called "rro"
+export RROPATH="${HOME}/rro"
 
 # Set the tag you want to merge
 export TAG="android-7.0.0_r14"
 
-do_not_merge="platform_vendor_aosp"
-# Orion manifest is setup with path first, then repo name, so the path attribute is after 2 spaces, and the name within "" in it
+do_not_merge="vendor/aosp manifest external/fuse packages/apps/Snap system/bt"
+
+cd ${RROPATH}
+
+# AOSP-RRO manifest is setup with path first, then repo name, so the path attribute is after 2 spaces, and the name within "" in it
 for repos in $(grep 'remote="rro"' ${RROPATH}/.repo/manifests/default.xml  | awk '{print $2}' | cut -d '"' -f2)
 do
 echo -e ""
@@ -25,12 +28,13 @@ else
 echo "$blu Merging $repos $end"
 echo -e ""
 cd $repos;
+git checkout nougat
 git fetch https://android.googlesource.com/platform/$repos -qt
 git merge $TAG
 if [ $? -ne 0 ];
 then
 echo "$repos" >> ${RROPATH}/failed
-echo "$red $repos failed :C $end"
+echo "$red $repos failed :( $end"
 else
 echo "$repos" >> ${RROPATH}/success
 echo "$grn $repos succeeded $end"
@@ -40,9 +44,24 @@ cd ${RROPATH};
 fi
 done
 
+cd packages/apps/Snap
+git checkout nougat
+git pull https://github.com/CyanogenMod/android_packages_apps_Snap cm-14.0
+cd ../../../
+cd system/bt
+git checkout nougat
+git pull https://github.com/CyanogenMod/android_system_bt.git cm-14.0
+cd ../../
+cd external/fuse
+git checkout nougat
+git pull https://github.com/CyanogenMod/android_external_fuse.git cm-14.0
+cd ../../
+
 echo -e ""
 echo -e "$red These repos failed $end"
 cat ./failed
 echo -e ""
 echo -e "$grn These succeeded $end"
 cat ./success
+
+
