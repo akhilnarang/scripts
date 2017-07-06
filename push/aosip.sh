c17619d2b35a93c793135db3000d983d1dd6e26a
Akhil Narang <akhilnarang.1999@gmail.com>
2017-07-06 13:39:02 +0200

    merge: misc improvements
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- push/aosip.sh
+++ push/aosip.sh
@@ -4,22 +4,23 @@
 
 if [ -z "$1" ];
 then
-export BRANCH="nougat"
+export BRANCH="nougat-mr2"
 else
 export BRANCH="$1"
 fi
 
-export AOSIP_SOURCE_DIR="${HOME}/aosip"
+export AOSIP_SOURCE_DIR="${HOME}/nougat"
 export DIR=$PWD
 
 cd ${AOSIP_SOURCE_DIR}
 
-PROJECTS="$(grep aosip .repo/manifests/manifests/aosip.xml | awk '{print $3}' | awk -F'"' '{print $2}')"
+PROJECTS="$(grep aosip .repo/manifests/snippets/aosip.xml | awk '{print $2}' | awk -F'"' '{print $2}')"
 
 
 for project in ${PROJECTS}; do
 cd $project;
-git push $(git remote -v | head -1 | awk '{print $2}' | sed -e 's/https:\/\/github.com\/AOSiP/ssh:\/\/localhost:29418\/AOSIP/') HEAD:nougat;
+#git push $(git remote -v | head -1 | awk '{print $2}' | sed -e 's/https:\/\/github.com\/AOSiP/ssh:\/\/localhost:29418\/AOSIP/') HEAD:nougat;
+git push aosip ${BRANCH};
 cd -;
 done
 
