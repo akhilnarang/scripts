bc6a62d6426045f2a0dbc20a5e3182065f404cf5
DroidFreak32 <rushabshah32@gmail.com>
2017-08-16 02:08:23 +0530

    Update arch-manjaro-apricity-build-environment-setup.sh
    Add a --noconfirm flag cuz lazy af.
    phablet-tools from aur causes error: "A failure occurred in package()."
    So use the part from ubuntu's script to install the necessary packages.

--- setup/arch-manjaro-apricity-build-environment-setup.sh
+++ setup/arch-manjaro-apricity-build-environment-setup.sh
@@ -15,10 +15,27 @@ ca-certificates-mozilla fakeroot make pkg-config
 sudo pacman -S gcc-multilib lib32-zlib lib32-ncurses lib32-readline
 # Disable pgp checking when installing stuff from AUR
 export MAKEPKG="makepkg --skippgpcheck"
-yaourt -S libtinfo
-yaourt -S lib32-ncurses5-compat-libs
-yaourt -S ncurses5-compat-libs
-yaourt -S phablet-tools
+yaourt -S libtinfo --noconfirm
+yaourt -S lib32-ncurses5-compat-libs --noconfirm
+yaourt -S ncurses5-compat-libs --noconfirm
+
+if [ -d "utils" ]; then
+	if [ "$(command -v make)" ]; then
+		makeversion="$(make -v | head -1 | awk '{print $3}')";
+		if [ "${makeversion}" != "4.2.1" ]; then
+			echo "Installing make 4.2.1 instead of ${makeversion}";
+			sudo install utils/make /usr/local/bin/;
+		fi
+	fi
+	echo "Installing repo";
+	sudo install utils/repo /usr/local/bin/;
+	echo "Installing ccache 3.3.4, please make sure your ROM includes the commit to use host ccache";
+	sudo install utils/ccache /usr/local/bin/;
+	echo "Installing ninja 1.7.2, please make sure your ROM includes the commit to use host ninja";
+	sudo install utils/ninja /usr/local/bin/;
+else
+	echo "Please run the script from root of cloned repo!";
+fi
 
 echo "All Done :'D"
 echo "Don't forget to run these command before building!"
