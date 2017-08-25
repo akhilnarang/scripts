42e4fe9ee34dbbe98c3d492ca5177462250ccb02
Akhil Narang <akhilnarang.1999@gmail.com>
2017-08-25 12:07:42 +0530

    Or not...

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 sudo install utils/hub /usr/local/bin/hub
 git config --global user.name "Akhil Narang"
 git config --global user.email "akhilnarang.1999@gmail.com"
-echo "\nsource ~/scripts/startupstuff.sh\nonLogin" >> ~/.bashrc
+echo "" >> ~/.bashrc
+echo "source ~/scripts/startupstuff.sh" >> ~/.bashrc
+echo "onLogin" >> ~/.bashrc
 sudo apt install figlet fortune -y
 
