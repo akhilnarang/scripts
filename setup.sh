32130436f61b4ff47e55a464b566c9c635aa4b54
Akhil Narang <akhilnarang.1999@gmail.com>
2017-08-25 12:05:11 +0530

    scripts: add a basic setup.sh xD
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo install utils/hub /usr/local/bin/hub
+git config --global user.name "Akhil Narang"
+git config --global user.email "akhilnarang.1999@gmail.com"
+echo "\nsource ~/scripts/startupstuff.sh\nonLogin" >> ~/.bashrc
+sudo apt install figlet fortune -y
+
