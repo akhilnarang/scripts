05877e1ffb6ae6dfe357c59f4e068fa3b653daf4
anayw2001 <anay1018@gmail.com>
2017-09-17 09:57:09 -0700

    clean up

---
+++ setup/fedora_buildenvsetup.sh
@@ -0,0 +1,5 @@
+sudo dnf install autoconf213 bison bzip2 ccache curl flex gawk gcc-c++ git glibc-devel
+sudo dnf install glibc-static libstdc++-static libX11-devel make mesa-libGL-devel ncurses-devel patch zlib-devel 
+sudo dnf install ncurses-devel.i686 readline-devel.i686 zlib-devel.i686 libX11-devel.i686 mesa-libGL-devel.i686 
+sudo dnf install glibc-devel.i686 libstdc++.i686 libXrandr.i686 zip perl-Digest-SHA wget lzop openssl-devel
+echo -e ("INSTALLED EVERYTHING!")
