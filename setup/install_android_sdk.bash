939b46590e6dc95515c264c0109e7ce016ace7a1
Harsh Shandilya <msfjarvis@gmail.com>
2017-10-05 21:56:18 +0530

    scripts: update Android SDK Setup script
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/install_android_sdk.bash
+++ setup/install_android_sdk.bash
@@ -2,7 +2,7 @@
 
 # Script to setup the Android SDK on a Linux System
 
-tools_url="https://dl.google.com/android/repository/tools_r25.2.3-linux.zip"
+tools_url="https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip"
 zip_name=$(printf '%s\n' "${tools_url##*/}")
 mkdir -p ~/Android/Sdk/ && cd ~/Android/Sdk
 wget $tools_url
@@ -10,10 +10,10 @@ wget $tools_url
 rm $zip_name
 echo 'export ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
 export ANDROID_HOME=~/Android/Sdk
-echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter android-25
+echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter android-26
 echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter extra-android-m2repository
 echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter extra-google-m2repository
-echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter build-tools-25.0.2
+echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter build-tools-26.0.2
 echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter tools
 echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter platform-tool
 echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter tools
