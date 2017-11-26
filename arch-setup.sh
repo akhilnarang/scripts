c049102d9cc5da2cc0b2c6a34c05f78041559d5d
Akhil Narang <akhilnarang.1999@gmail.com>
2017-11-26 13:02:17 +0530

    Update some stuff :D
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ arch-setup.sh
@@ -0,0 +1,7 @@
+git config --global user.name "Akhil Narang"
+git config --global user.email "akhilnarang.1999@gmail.com"
+echo "" >> ~/.bashrc
+echo "source ~/scripts/startupstuff.sh" >> ~/.bashrc
+echo "onLogin" >> ~/.bashrc
+pacaur -S figlet fortune-mod hub
+
