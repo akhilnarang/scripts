00d6860eeb59e31555c34c4462deb491321aaf3d
Akhil Narang <akhilnarang.1999@gmail.com>
2017-11-26 13:08:46 +0530

    setup: update archlinux script, simplify a lot.
    Thanks to https://wiki.archlinux.org/index.php/android#Building_Android
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/arch-manjaro.sh
+++ setup/arch-manjaro.sh
@@ -5,45 +5,22 @@
 clear
 echo Installing Dependencies!
 # Update
-sudo pacman -Syu
-# Install needed packages
-sudo pacman -S gcc git gnupg flex bison gperf sdl wxgtk bash-completion \
-squashfs-tools curl ncurses zlib schedtool perl-switch zip \
-unzip libxslt maven tmux screen w3m python2-virtualenv bc rsync ncftp \
-ca-certificates-mozilla fakeroot make pkg-config
-# Installing 64 bit needed packages
-sudo pacman -S gcc-multilib lib32-zlib lib32-ncurses lib32-readline
-# Disable pgp checking when installing stuff from AUR
-export MAKEPKG="makepkg --skippgpcheck"
-yaourt -S libtinfo --noconfirm
-yaourt -S lib32-ncurses5-compat-libs --noconfirm
-yaourt -S ncurses5-compat-libs --noconfirm
+sudo pacman -Syyu
+# Install pacaur
+sudo pacman -S pacaur
 # Downgrade curl for now
-yaourt -S agetpkg-git --noconfirm
+pacaur -S agetpkg-git --noconfirm
 agetpkg --install curl 7.55.1 1
-sudo pacman -R agetpkg-git
 sudo sed -i '/\[options\]/a IgnorePkg = curl' /etc/pacman.conf
-
-if [ -d "utils" ]; then
-	if [ "$(command -v make)" ]; then
-		makeversion="$(make -v | head -1 | awk '{print $3}')";
-		if [ "${makeversion}" != "4.2.1" ]; then
-			echo "Installing make 4.2.1 instead of ${makeversion}";
-			sudo install utils/make /usr/local/bin/;
-		fi
-	fi
-	echo "Installing repo";
-	sudo install utils/repo /usr/local/bin/;
-	echo "Installing ccache 3.3.4, please make sure your ROM includes the commit to use host ccache";
-	sudo install utils/ccache /usr/local/bin/;
-	echo "Installing ninja 1.7.2, please make sure your ROM includes the commit to use host ninja";
-	sudo install utils/ninja /usr/local/bin/;
-else
-	echo "Please run the script from root of cloned repo!";
-fi
+# Import PGP signatures for ncurses5-compat-libs and lib32-ncurses5-compat-libs
+gpg --recv-keys 702353E0F7E48EDB
+# Install aosp-devel (and lineageos-devel because quite a few probably build Lineage/Lineage based ROMs as well.
+pacaur -S aosp-devel lineageos-devel --noconfirm
+# Just a couple of other useful tools I use, others do too probably
+pacaur -S hub neofetch fortune-mod --noconfirm
 
 echo "All Done :'D"
-echo "Don't forget to run these command before building!"
+echo "Don't forget to run these commands before building, or make sure the python in your PATH is python2 and not python3"
 echo "
 virtualenv2 venv
 source venv/bin/activate
