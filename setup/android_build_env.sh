66d5dced92c44b12b2e6a701ad9a901c5b690014
Akhil Narang <akhilnarang.1999@gmail.com>
2018-06-13 13:30:01 +0530

    setup: hopefully fix this
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -9,25 +9,22 @@
 LATEST_MAKE_VERSION="4.2.1"
 UBUNTU_14_PACKAGES="git-core libesd0-dev libwxgtk2.8-dev curl schedtool binutils-static figlet libesd0-dev"
 UBUNTU_16_PACKAGES="libesd0-dev"
-CORE_PACKAGES="python gnupg flex bison gperf libsdl1.2-dev squashfs-tools build-essential zip libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk
-pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib lib32z1-dev lib32ncurses5-dev gcc-multilib liblz4-* pngquant
-ncurses-dev texinfo gcc gperf patch libtool automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev libcap-dev
-autoconf libgmp-dev build-essential pkg-config libmpc-dev libmpfr-dev lzma* liblzma* w3m adb fastboot maven ncftp htop imagemagick
-libssl-dev clang cmake"
 PACKAGES=""
 
 LSB_RELEASE=$(lsb_release -d)
 
-if [[ ${LSB_RELEASE} =~ "Mint 19" || ${LSB_RELEASE} =~ "Ubuntu 18" ]]; then
-    PACKAGES="${CORE_PACKAGES}"
-elif [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
-    PACKAGES="${CORE_PACKAGES} ${UBUNTU_16_PACKAGES}"
+if [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
+    PACKAGES="${UBUNTU_16_PACKAGES}"
 elif [[ ${LSB_RELEASE} =~ "Ubuntu 14" ]]; then
-    PACKAGES="${CORE_PACKAGES} ${UBUNTU_14_PACKAGES}"
+    PACKAGES="${UBUNTU_14_PACKAGES}"
 fi
 
 sudo apt update -y
-sudo apt install "${PACKAGES}"
+sudo apt install python gnupg flex bison gperf libsdl1.2-dev squashfs-tools build-essential zip libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk \
+pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib lib32z1-dev lib32ncurses5-dev gcc-multilib liblz4-* pngquant \
+ncurses-dev texinfo gcc gperf patch libtool automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev libcap-dev \
+autoconf libgmp-dev build-essential pkg-config libmpc-dev libmpfr-dev lzma* liblzma* w3m adb fastboot maven ncftp htop imagemagick \
+libssl-dev clang cmake "${PACKAGES}"
 
 if [[ ! "$(command -v adb)" == "" ]]; then
     echo -e "Setting up udev rules for adb!"
