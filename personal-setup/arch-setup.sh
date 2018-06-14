e3abf414e20469f895814fa4cb1092ae354f8760
Akhil Narang <akhilnarang.1999@gmail.com>
2018-06-14 22:03:16 +0530

    scripts: miscellaneous cleanup
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- personal-setup/arch-setup.sh
+++ personal-setup/arch-setup.sh
@@ -2,7 +2,7 @@ git config --global user.name "Akhil Narang"
 git config --global user.email "akhilnarang.1999@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
 echo "" >> ~/.bashrc
-echo "source ~/scripts/startupstuff.sh" >> ~/.bashrc
+echo "source ~/scripts/functions" >> ~/.bashrc
 echo "onLogin" >> ~/.bashrc
 yaourt -S figlet fortune-mod hub --noconfirm
 
