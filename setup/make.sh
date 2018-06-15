8a64f1b6f9bfc4aecc8ee82e2590244727bf8bff
Akhil Narang <akhilnarang.1999@gmail.com>
2018-06-15 19:18:12 +0530

    setup: make: restore to previous working version
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/make.sh
+++ setup/make.sh
@@ -1,5 +1,7 @@
-tar xvz -C /tmp/ < <(axel -q -o - https://ftp.gnu.org/gnu/make/make-${1}.tar.gz)
-cd /tmp/make-${1};
+cd /tmp
+axel -a -n 10 https://ftp.gnu.org/gnu/make/make-${1}.tar.gz
+tar xvzf /tmp/make-${1}.tar.gz
+cd /tmp/make-${1} || exit 1;
 ./configure;
 bash ./build.sh;
 sudo install ./make /usr/local/bin/make;
