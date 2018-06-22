9654f6ec73fe94c6e303bf75f390a73f6052a797
Yuvraj <frap13000@gmail.com>
2018-06-22 14:32:56 +0530

    Setup: remove make tarball after extracting
    Signed-off-by: Yuvraj Saxena <frap13000@gmail.com>

--- setup/make.sh
+++ setup/make.sh
@@ -1,6 +1,7 @@
 cd /tmp
 axel -a -n 10 https://ftp.gnu.org/gnu/make/make-${1}.tar.gz
 tar xvzf /tmp/make-${1}.tar.gz
+rm /tmp/make-${1}.tar.gz
 cd /tmp/make-${1} || exit 1
 ./configure
 bash ./build.sh
