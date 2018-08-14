98ec6139ad02d533b8742dbc02777f48d0ae8cd7
Akhil Narang <akhilnarang.1999@gmail.com>
2018-08-14 15:23:23 +0530

    make: apply a patch to fix build
      * Been getting the following error recently
    
    linking make...
    glob.o: In function `glob_in_dir':
    /tmp/make-4.2.1/./glob/glob.c:1367: undefined reference to `__alloca'
    /tmp/make-4.2.1/./glob/glob.c:1342: undefined reference to `__alloca'
    /tmp/make-4.2.1/./glob/glob.c:1283: undefined reference to `__alloca'
    /tmp/make-4.2.1/./glob/glob.c:1256: undefined reference to `__alloca'
    glob.o: In function `glob':
    /tmp/make-4.2.1/./glob/glob.c:581: undefined reference to `__alloca'
    glob.o:/tmp/make-4.2.1/./glob/glob.c:732: more undefined references to `__alloca' follow
    collect2: error: ld returned 1 exit status
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/make.sh
+++ setup/make.sh
@@ -3,6 +3,7 @@ axel -a -n 10 https://ftp.gnu.org/gnu/make/make-${1}.tar.gz
 tar xvzf /tmp/make-${1}.tar.gz
 cd /tmp/make-${1} || exit 1
 ./configure
+curl https://raw.githubusercontent.com/akhilnarang/scripts/master/patches/make-glibc_alloc_fix.patch | patch -p1
 bash ./build.sh
 sudo install ./make /usr/local/bin/make
 cd -
