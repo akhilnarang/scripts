akhilnarang/scripts

commit 252aca688e07de2a80fa6881b30eb1491ddeadd5

setup: Android SDK installation improvements
- Cleanup coding style
- Add a script to install a set of default packages that should
  satisfy most use cases.

setup/install_android_sdk.bash
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # Script to setup the Android SDK on a Linux System
-
-tools_url="https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip"
-zip_name=$(printf '%s\n' "${tools_url##*/}")
-mkdir -p ~/Android/Sdk/ && cd ~/Android/Sdk
-axel -a -n 10 $tools_url
-[ $? -eq 0 ] && unzip $zip_name || exit 1
-rm $zip_name
+SCRIPT_DIR="$(cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd)"
+TOOLS_URL="https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip"
+ZIP_NAME=$(printf '%s\n' "${TOOLS_URL##*/}")
+mkdir -p ~/Android/Sdk/; cd ~/Android/Sdk || exit 1
+axel -a -n 10 "${TOOLS_URL}"
+[ "${?}" -eq 0 ] && unzip "${ZIP_NAME}" || exit 1
+rm "${ZIP_NAME}"
 echo 'export ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
-export ANDROID_HOME=~/Android/Sdk
-yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
+source ~/.bashrc
+yes | "${ANDROID_HOME}"/tools/bin/sdkmanager --licenses
+
+source "${SCRIPT_DIR}"/setup_android_sdk_packages.sh "${SCRIPT_DIR}"

setup/setup_android_sdk_packages.bash
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+# Actually install some packages too so this thing isn't useless
+while read p; do
+    "${ANDROID_HOME}"/tools/bin/sdkmanager "${p}"
+done < "${1:?}"/android-sdk-minimal.txt

commit 55a5997b25e0157f16b460850814e5036721f097

setup: Fix derps in SDK install overhaul
Uncommitted changes strike again

Fixes: 252aca688e07 ("setup: Android SDK installation improvements")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

setup/install_android_sdk.bash
@@ -12,4 +12,4 @@ echo 'export ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
 source ~/.bashrc
 yes | "${ANDROID_HOME}"/tools/bin/sdkmanager --licenses
 
-source "${SCRIPT_DIR}"/setup_android_sdk_packages.sh "${SCRIPT_DIR}"
+source "${SCRIPT_DIR}"/setup_android_sdk_packages.bash "${SCRIPT_DIR}"