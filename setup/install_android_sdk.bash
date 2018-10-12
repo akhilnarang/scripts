835006816aa792e803876432112529e465eb85ed
Harsh Shandilya <harsh@prjkt.io>
2018-10-12 14:40:34 +0530

    setup: install_android_sdk: fexing da bugz u'll never see
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- setup/install_android_sdk.bash
+++ setup/install_android_sdk.bash
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    echo "This script relies on information from your environment and thus should be sourced."
+    exit 1
+fi
+
 # Script to setup the Android SDK on a Linux System
-SCRIPT_DIR="$(cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd)"
+CUR_DIR="$(cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd)"
+CUR_DIR="${CUR_DIR/setup/}"
 TOOLS_URL="https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip"
 ZIP_NAME=$(printf '%s\n' "${TOOLS_URL##*/}")
 mkdir -p ~/Android/Sdk/; cd ~/Android/Sdk || exit 1
 if axel -a -n 10 "${TOOLS_URL}"; then
-    unzip "${ZIP_NAME}"
+    unzip -o "${ZIP_NAME}"
 else
     exit 1
 fi
 rm "${ZIP_NAME}"
-echo 'export ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
+printf '\nexport ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
 source ~/.bashrc
 yes | "${ANDROID_HOME}"/tools/bin/sdkmanager --licenses
 
-source "${SCRIPT_DIR}"/setup_android_sdk_packages.bash "${SCRIPT_DIR}"
+source "${CUR_DIR}"/setup/setup_android_sdk_packages.bash "${CUR_DIR}/setup"
+cd "${CUR_DIR}"
