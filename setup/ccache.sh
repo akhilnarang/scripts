1f311ad5723761b19d01020d5355456183278e49
Akhil Narang <akhilnarang.1999@gmail.com>
2018-11-04 01:57:57 +0530

    ccache: don't build man pages
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/ccache.sh
+++ setup/ccache.sh
@@ -4,7 +4,7 @@ cd /tmp || exit 1
 git clone git://github.com/akhilnarang/ccache.git
 cd ccache || exit 1
 ./autogen.sh
-./configure
+./configure --disable-man
 make -j"$(nproc)"
 sudo make install
 rm -rf "${PWD}"
