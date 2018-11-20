5a4c75e558156580b085017db661c8363720b484
Harsh Shandilya <msfjarvis@gmail.com>
2018-11-20 13:06:22 +0530

    install re2c for ninja
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -24,7 +24,7 @@ sudo apt install -y python gnupg flex bison gperf libsdl1.2-dev squashfs-tools b
 pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev g++-multilib lib32z1-dev lib32ncurses5-dev gcc-multilib liblz4-* pngquant \
 ncurses-dev texinfo gcc gperf patch libtool automake g++ gawk subversion expat libexpat1-dev python-all-dev bc libcloog-isl-dev libcap-dev \
 autoconf libgmp-dev build-essential pkg-config libmpc-dev libmpfr-dev lzma* liblzma* w3m adb fastboot maven ncftp htop imagemagick \
-libssl-dev clang cmake axel "${PACKAGES}"
+libssl-dev clang cmake axel re2c "${PACKAGES}"
 
 if [[ ! "$(command -v adb)" == "" ]]; then
     echo -e "Setting up udev rules for adb!"
