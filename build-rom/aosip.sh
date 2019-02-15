881906ebfbe6561060dbf53ed1fd8cdc93549ec0
Akhil Narang <akhilnarang.1999@gmail.com>
2019-02-15 16:31:19 +0530

    build-rom: add updated AOSiP build script
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- build-rom/aosip.sh
+++ build-rom/aosip.sh
@@ -1,65 +1,49 @@
 #!/usr/bin/env bash
 
-if [ -z $1 ]
-then
-DEVICE="oneplus3"
-else
-DEVICE=$1
+# Copyright (C) 2018 Akhil Narang
+# SPDX-License-Identifier: GPL-3.0-only
+# AOSiP Build Script
+# shellcheck disable=SC1090,SC1091
+# SC1090: Can't follow non-constant source. Use a directive to specify location.
+# SC1091: Not following: (error message here)
+
+set -e
+source ~/scripts/functions
+sendAOSiP "${START_MESSAGE}";
+export PATH=~/bin:$PATH
+sendAOSiP "Starting ${DEVICE} ${AOSIP_BUILDTYPE} build on ${node:?}, check progress [here](${BUILD_URL})!"
+if [[ "${SYNC}" == "yes" ]]; then
+	repo init -u https://github.com/AOSiP/platform_manifest.git -b pie --no-tags --no-clone-bundle --current-branch --repo-url https://github.com/akhilnarang/repo --repo-branch master --no-repo-verify;
+	set +e;
+	rm -rfv .repo/local_manifests/
+	set -e;
+	time repo sync -j32 --current-branch --no-tags --no-clone-bundle --force-sync
 fi
-
-source build/envsetup.sh
-
-if [[ "$2" =~ "clean" ]]
-then
-make clean
-fi
-
-if [[ "$2" =~ "dirty" ]]
-then
-make installclean
-outdir="./out/target/product/${DEVICE}"
-  rm -rf "$outdir/combinedroot"
-  rm -rf "$outdir/data"
-  rm -rf "$outdir/recovery"
-  rm -rf "$outdir/root"
-  rm -rf "$outdir/system"
-  rm -rf "$outdir/utilities"
-  rm -rf "$outdir/boot"*
-  rm -rf "$outdir/combined"*
-  rm -rf "$outdir/kernel"
-  rm -rf "$outdir/ramdisk"*
-  rm -rf "$outdir/recovery"*
-  rm -rf "$outdir/system"*
-  rm -rf "$outdir/obj/ETC/system_build_prop_intermediates"
-  rm -rf "$outdir/ota_temp/RECOVERY/RAMDISK"
-fi
-
-if [[ "$2" =~ "sync" ]]
-then
-time repo sync -c -f -j16 --force-sync --no-clone-bundle --no-tags
-fi
-
-export CCACHE_DIR=${HOME}/.ccache-${DEVICE}
+set +e
+. build/envsetup.sh
+lunch aosip_"${DEVICE}"-"${BUILDVARIANT}"
+set -e
+case "${CLEAN}" in
+  "clean"|"deviceclean"|"installclean") m -j "${CLEAN}" ;;
+  *) rm -rf "${OUT}"/A*
+esac
+repopick_stuff
+eval "${COMMAND_TO_RUN}"
 export USE_CCACHE=1
-if [[ "${DEVICE}" = "oneplus3" ]]; then
-    ./prebuilts/misc/linux-x86/ccache/ccache -M 100
-else
-    ./prebuilts/misc/linux-x86/ccache/ccache -M 30
-fi
-
-rm -rfv .repo/local_manifests/
-lunch aosip_${DEVICE}-userdebug
-time mka kronic
-if [ $? -eq 0 ]; then
-  cd $OUT
-  AOSIP_ZIP="$(ls AOSiP*.zip)"
-  rsync -av "${AOSIP_ZIP}" "akhil@aosiprom.com:/home/kronic/aosiprom.com/.dothidden/akhil/${DEVICE}/"
-  cd -
-  DOWNLOAD_URL="http://aosiprom.com/.dothidden/akhil/${DEVICE}/${AOSIP_ZIP}"
-  bash ~/kronicbot/aosip_testers.sh "[$AOSIP_ZIP](${DOWNLOAD_URL})"
-  if [[ "${DEVICE}" = "oneplus3" ]]; then
-    bash ~/kronicbot/3_t_testers.sh "[$AOSIP_ZIP](${DOWNLOAD_URL})"
-  fi
-else
-    bash ~/kronicbot/aosip_testers.sh "Failed: ${DEVICE} ${AOSIP_BUILDTYPE}"
-fi
+export CCACHE_DIR="${HOME}/.ccache"
+ccache -M 200G
+time m -j kronic || sendAOSiP "Build failed!"
+set +e;
+ZIP="$(cout && ls AOSiP*.zip)" || exit 1
+sendAOSiP "Build done, check ${BUILD_URL} for details!"
+while [[ ! "$url" =~ "https://drive.google.com" ]]; do
+	url="$(gdrive upload -p 1hhyKQ9yqLg0bIn-QmkPhpMrrc7OuHuNC "${OUT}"/"${ZIP}" --share | tail -1 | awk '{ print $NF }')"
+done
+MD5="$(md5sum "${OUT}"/"${ZIP}" | awk '{print $1}')"
+SIZE="$(du -sh "${OUT}"/"${ZIP}" | awk '{print $1}')"
+sendAOSiP "
+[$ZIP]($url)
+Size: $SIZE
+MD5: \`$MD5\`
+"
+sendAOSiP "${END_MESSAGE}";
