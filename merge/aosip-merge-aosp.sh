cb87ac0cf8caeddb68d67d03cae4154e5f15e89c
Akhil Narang <akhilnarang.1999@gmail.com>
2019-04-02 14:52:18 +0530

    scripts: merge: I don't use jenkins for this anymore
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- merge/aosip-merge-aosp.sh
+++ merge/aosip-merge-aosp.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 AOSIP_PATH=$PWD
-bash ~/kronicbot/send_tg_nomd.sh -1001055786180 "Merging in ${TAG}"
-bash ~/kronicbot/send_tg.sh -1001055786180 "Check progress [here]($BUILD_URL)!"
-git config --global user.name "Akhil's Lazy Buildbot"
-git config --global user.email "jenkins@akhilnarang.me"
-git config --global user.signingkey 219187E8
+TAG="android-9.0.0_r${1:?}"
+SRC="pie"
 
 do_not_merge="vendor/aosip manifest updater packages/apps/OmniSwitch packages/apps/OmniStyle \
 packages/apps/OwlsNest external/google packages/apps/Launcher3 hardware/qcom/power \
@@ -73,9 +70,3 @@ for repos in $(grep 'remote="aosip"' ${AOSIP_PATH}/.repo/manifests/snippets/aosi
 done
 
 FAILED=$(cat $AOSIP_PATH/failed)
-bash ~/kronicbot/send_tg.sh "-1001055786180" "Failed repos:"
-bash ~/kronicbot/send_tg.sh "-1001055786180" $FAILED
-
-git config --global user.name "Akhil Narang"
-git config --global user.email "akhilnarang.1999@gmail.com"
-git config --global user.signingkey 944082E8
