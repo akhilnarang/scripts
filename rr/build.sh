e4955445a91386cc4942502c5687067929e5a8f0
Akhil Narang <akhilnarang.1999@gmail.com>
2019-04-28 22:23:23 +0530

    script: rr: update build script
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- rr/build.sh
+++ rr/build.sh
@@ -4,18 +4,31 @@
 # SC1091: Not following: build/envsetup.sh: openBinaryFile: does not exist (No such file or directory)
 # SC2029: Note that, unescaped, this expands on the client side
 
+# Repopicks a | delimited set of commits
+function repopick_stuff() {
+	export oldifs=$IFS
+	export IFS="|"
+	for f in ${REPOPICK_LIST}; do
+	    echo "Picking: $f"
+	    eval repopick "${f}" || return
+	done
+	export IFS=$oldifs
+}
+
 [[ -z "${API_KEY}" ]] && echo "API_KEY not defined, exiting!" && exit 1
 function sendTG() {
     curl -s "https://api.telegram.org/bot${API_KEY}/sendmessage" --data "text=${*}&chat_id=-1001185331716&parse_mode=Markdown" > /dev/null
 }
 rm -fv .repo/local_manifests/*
 export days_to_log=${DAY}
+source ~/.bashrc
 repo sync --force-sync -j64
 . build/envsetup.sh
 if ! breakfast "${DEVICE}"; then
 	sendTG "Lunching ${DEVICE} failed on $NODE_NAME"
     exit 1
 fi
+repopick_stuff
 export USE_CCACHE=1 
 ccache -M 200G
 mka "${CLOBBER:?}"
@@ -27,12 +40,12 @@ if ! mka bacon; then
 fi
 cout
 ZIP=$(ls RR*.zip)
-ssh acar@ssh.packet.resurrectionremix.com "rm -rf ~/ua/.hidden/${DEVICE:?}"
-ssh acar@ssh.packet.resurrectionremix.com "mkdir ~/ua/.hidden/$DEVICE -p"
-scp "${ZIP}" acar@ssh.packet.resurrectionremix.com:ua/.hidden/"${DEVICE}"/
-scp "${DEVICE}".json acar@ssh.packet.resurrectionremix.com:ua/.hidden/"${DEVICE}"/
+ssh jenkins@ssh.packet.resurrectionremix.com "rm -rf /home/acar/ua/.hidden/${DEVICE:?}"
+ssh jenkins@ssh.packet.resurrectionremix.com "mkdir /home/acar/ua/.hidden/$DEVICE -p"
+scp "${ZIP}" "jenkins@ssh.packet.resurrectionremix.com:/home/acar/ua/.hidden/${DEVICE}"/
+scp "${DEVICE}".json "jenkins@ssh.packet.resurrectionremix.com:/home/acar/ua/.hidden/${DEVICE}"/
 cd - || exit
-scp CHANGELOG.mkdn acar@ssh.packet.resurrectionremix.com:ua/.hidden/"${DEVICE}/${ZIP/.zip/-changelog.txt}"
+scp CHANGELOG.mkdn "jenkins@ssh.packet.resurrectionremix.com:/home/acar/ua/.hidden/${DEVICE}/${ZIP/.zip/-changelog.txt}"
 DEVICE_C="$(echo "$DEVICE" | tr a-z A-Z)"
 sendTG "$DEVICE_C build is done. It's private. Test it and let us know if we can publish it."
 sendTG "[$ZIP](https://rr.umutcanacar.me/.hidden/$DEVICE/$ZIP)"
