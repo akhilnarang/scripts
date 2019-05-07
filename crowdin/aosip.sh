67835efe25523fd369e9326d4e7b8764eb8fb7ef
Akhil Narang <akhilnarang.1999@gmail.com>
2019-05-07 14:55:45 +0530

    scripts: crowdin: add aosip script
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ crowdin/aosip.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+source ~/.rvm/scripts/rvm
+repo init -u git://github.com/AOSiP/platform_manifest.git -b pie -m crowdin.xml --no-tags --current-branch
+time repo sync -j8 --force-sync --force-broken --no-tags --current-branch
+repo forall -j8 -c "scp -p -P 29418 review.aosiprom.com:hooks/commit-msg .git/hooks/"
+./crowdin/sync.py
