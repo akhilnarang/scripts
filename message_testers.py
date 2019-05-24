0a64cc71063bc7839745039020c244170c3f82dd
Akhil Narang <akhilnarang.1999@gmail.com>
2019-05-24 16:06:11 +0530

    scripts: message_testers: fix tagging if only one tester
    @n @e @z @o @r @f @l @a @m @e for fajita wasn't very effective in tagging him
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- message_testers.py
+++ message_testers.py
@@ -7,25 +7,25 @@ if len(argv) != 2:
     exit(1)
 
 testers = {
-    'berkeley': ('srisurya95'),
-    'beryllium': ('akhilnarang', 'grewal'),
-    'bullhead': ('anirudhgupta109', 'Skittles8923'),
-    'cheeseburger': ('HydroPetro', 'fryevia'),
-    'crosshatch': ('XlxFoXxlX'),
-    'dipper': ('argraur'),
-    'dumpling': ('divadsn', 'HydroPetro'),
-    'enchilada': ('anirudhgupta109'),
-    'fajita': ('nezorflame'),
-    'kiwi': ('coldhans'),
-    'mata': ('KuranKaname', 'XlxFoXxlX', 'Y45HW4N7'),
-    'mido': ('Adesh15'),
-    'oneplus3': ('HydroPetro', 'theshinybeast'),
-    'platina': ('nysadev'),
-    'potter': ('NickvBokhorst'),
-    'whyred': ('akhilnarang', 'iwantz', 'raiadventures', 'ahmed_tohamy', 'MSFJarvis', 'Sanchith_Hegde', 'ai94iq', 'anunaym14_bot', 'bohrabhijeet', 'ZTR23'),
-    'x2': ('moto999999'),
-    'z2_plus': ('kenny3fcb', 'Pavan_Paps'),
-    'zl1': ('Gabronog')
+    'berkeley': ['srisurya95'],
+    'beryllium': ['akhilnarang', 'grewal'],
+    'bullhead': ['anirudhgupta109', 'Skittles8923'],
+    'cheeseburger': ['HydroPetro', 'fryevia'],
+    'crosshatch': ['XlxFoXxlX'],
+    'dipper': ['argraur'],
+    'dumpling': ['divadsn', 'HydroPetro'],
+    'enchilada': ['anirudhgupta109'],
+    'fajita': ['nezorflame'],
+    'kiwi': ['coldhans'],
+    'mata': ['KuranKaname', 'XlxFoXxlX', 'Y45HW4N7'],
+    'mido': ['Adesh15'],
+    'oneplus3': ['HydroPetro', 'theshinybeast'],
+    'platina': ['nysadev'],
+    'potter': ['NickvBokhorst'],
+    'whyred': ['akhilnarang', 'iwantz', 'raiadventures', 'ahmed_tohamy', 'MSFJarvis', 'Sanchith_Hegde', 'ai94iq', 'anunaym14_bot', 'bohrabhijeet', 'ZTR23'],
+    'x2': ['moto999999'],
+    'z2_plus': ['kenny3fcb', 'Pavan_Paps'],
+    'zl1': ['Gabronog']
 }
 
 device = argv[1]
