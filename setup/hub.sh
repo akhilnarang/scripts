60803de5c2c0c55862fbcb98428271ee746774c7
Harsh Shandilya <msfjarvis@gmail.com>
2019-05-28 21:18:06 +0530

    setup/hub: Import missing bits from my dotfiles and use wget
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup/hub.sh
+++ setup/hub.sh
@@ -3,6 +3,22 @@
 # Copyright (C) Harsh Shandilya <msfjarvis@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-only
 
+function get_release_assets () {
+    local REPOSITORY RELEASE_TAG RELEASE_ID TMP_FILE;
+    REPOSITORY="${1:?}";
+    RELEASE_TAG="${2:-latest}";
+    TMP_FILE="$(mktemp)";
+    if [ "${RELEASE_TAG}" == "latest" ]; then
+        RELEASE_ID=$(curl --silent "https://api.github.com/repos/${1:?}/releases/latest" | # Get the latest release from GitHub API
+        jq -r .id);
+    else
+        curl --silent "https://api.github.com/repos/${REPOSITORY}/releases" | jq '.[] | {id: .id, name: .tag_name}' > "${TMP_FILE}";
+        RELEASE_ID=$(jq -r '"\(.id) \(.name)"' "${TMP_FILE}" | grep "${RELEASE_TAG}" | awk '{print $1}');
+    fi;
+    curl --silent "https://api.github.com/repos/${REPOSITORY}/releases/${RELEASE_ID}" | jq -r .assets[].browser_download_url;
+    [ -f "${TMP_FILE}" ] && rm -f "${TMP_FILE}"
+}
+
 function get_latest_release {
   curl --silent "https://api.github.com/repos/${1:?}/releases/latest" | # Get latest release from GitHub API
     jq -r .tag_name                                                 # Get tag line
@@ -12,11 +28,11 @@ function install_hub {
     local INSTALLED_VERSION HUB HUB_ARCH CL_YLW CL_RST
     CL_YLW='\033[01;33m'
     CL_RST='\033[0m'
-    echoText "Checking and installing hub"
+    echo "Checking and installing hub"
     HUB="$(command -v hub)"
     HUB_ARCH=linux-amd64
     if [ -z "${HUB}" ]; then
-        aria2c "$(get_release_assets github/hub | grep ${HUB_ARCH})" -o hub.tgz
+        wget "$(get_release_assets github/hub | grep ${HUB_ARCH})" -O=hub.tgz
         mkdir -p hub
         tar -xf hub.tgz -C hub
         sudo ./hub/*/install --prefix=/usr/local/
@@ -26,7 +42,7 @@ function install_hub {
         LATEST_VERSION="$(get_latest_release github/hub)"
         if [ "${INSTALLED_VERSION}" != "${LATEST_VERSION}" ]; then
             echo -e "${CL_YLW} Outdated version of hub detected, upgrading${CL_RST}"
-            aria2c "$(get_release_assets github/hub | grep ${HUB_ARCH})" -o hub.tgz
+            wget "$(get_release_assets github/hub | grep ${HUB_ARCH})" -O=hub.tgz
             mkdir -p hub
             tar -xf hub.tgz -C hub
             sudo ./hub/*/install --prefix=/usr/local/
