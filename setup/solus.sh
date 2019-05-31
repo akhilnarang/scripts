cf50605bef0d470f79a66c7be568da431a31bc7e
Akhil Narang <akhilnarang.1999@gmail.com>
2019-05-31 23:37:06 +0530

    setup: solus: cleanup a bit
    Remove ccache prompts
    Change printf to echo statements
    Use repo from my fork
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/solus.sh
+++ setup/solus.sh
@@ -13,43 +13,14 @@ sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3r
 sudo eopkg it android-tools*.eopkg;sudo rm android-tools*.eopkg
 
 # udev rules
-echo -e "Setting up udev rules for adb!"
+echo "Setting up udev rules for adb!"
 sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
 sudo chmod 644 /etc/udev/rules.d/51-android.rules
 sudo chown root /etc/udev/rules.d/51-android.rules
 sudo usysconf run -f
 
 echo "Installing repo"
-sudo curl --create-dirs -L -o /usr/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
-sudo chmod a+x /usr/bin/repo
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://github.com/akhilnarang/repo/raw/master/repo
+sudo chmod a+x /usr/local/bin/repo
 
-printf "\nYou are now ready to build Android!"
-printf "\n\n"
-
-# CCACHE
-while true; do
-    read -p "Do you wish to enable CCACHE? (y/n)" yn
-    case $yn in
-        [Yy]* ) echo "export USE_CCACHE=1
-export CCACHE_DIR=${HOME}/.ccache" >> ${HOME}/.bashrc ; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-printf "\nTo enable CCACHE for a ROM, run this from your the top of your ROM's directory.\n"
-printf "prebuilts/misc/linux-x86/ccache/ccache -M 10G\n"
-printf "\n"	
-
-source ${HOME}/.bashrc
-
-while true; do
-    read -p "Do you also wish to compress CCACHE? (y/n)" yn
-    case $yn in
-        [Yy]* ) echo "export CCACHE_COMPRESS=1" >> ${HOME}/.bashrc ; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-source ${HOME}/.bashrc	
+echo "You are now ready to build Android!"
