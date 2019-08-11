9e694e76136e5f77bc6fcba69a82e5b6a1be7298
Akhil Narang <akhilnarang.1999@gmail.com>
2019-08-11 17:49:19 +0530

    scripts: extract_and_push: major updates
    Mostly squashed from https://github.com/AndroidDumps/dumpyara/commits/master/dumpyara.sh
    Thanks to Erfan and Shivam for their commits
    
    Cleaned up a few shellcheck warnings as well
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- misc/extract_and_push.sh
+++ misc/extract_and_push.sh
@@ -8,120 +8,104 @@ function sendTG() {
 
 [[ -z "$ORG" ]] && ORG="AndroidDumps"
 sendTG "Starting <a href=\"${URL:?}\">dump</a> on <a href=\"$BUILD_URL\">jenkins</a>"
-aria2c -x16 -j$(nproc) ${URL} || wget ${URL} || exit 1
-sendTG "Downloaded"
+aria2c -x16 -j"$(nproc)" "${URL}" || wget "${URL}" || exit 1
+sendTG "Downloaded the file"
 FILE=${URL##*/}
 EXTENSION=${URL##*.}
 UNZIP_DIR=${FILE/.$EXTENSION/}
 
-if [[ "${EXTENSION}" == "tgz" ]]; then
-    tar xf ${FILE}
-    cd */images
+PARTITIONS="system vendor cust odm oem factory product modem xrom systemex"
+
+ if [[ ! -d "${HOME}/extract_android_ota_payload" ]]; then
+    git clone https://github.com/cyxx/extract_android_ota_payload ~/extract_android_ota_payload
 else
-    if [[ -f "${FILE}" ]]; then
-        7z x ${FILE} -o${UNZIP_DIR}
-    else
-        7z x * -o${UNZIP_DIR}
-    fi
-
-    cd ${UNZIP_DIR} || exit 1
-    files=$(ls)
-    if [[ -d "${files}" ]] && [[ $(echo ${files} | wc -l) -eq 1 ]]; then
-        cd ${files}
-    fi
-    files=$(ls *.zip)
-    if [[ -f "${files}" ]] && [[ $(echo ${files} | wc -l) -eq 1 ]] && [[ "${files}" != "compatibility.zip" ]]; then
-        unzip ${files}
-    fi
-
-    if [[ -f "payload.bin" ]]; then
-        sendTG "payload detected"
-        if [[ ! -d "${HOME}/extract_android_ota_payload" ]]; then
-            cd
-            git clone https://github.com/cyxx/extract_android_ota_payload
-            cd -
-        fi
-        python2 ~/extract_android_ota_payload/extract_android_ota_payload.py payload.bin
-    fi
+    git -C ~/extract_android_ota_payload pl
 fi
-
-rm -fv $OLDPWD/*.zip
-
-for p in system vendor cust odm oem; do
-    brotli -d $p.new.dat.br &>/dev/null ; #extract br
-    cat $p.new.dat.{0..999} 2>/dev/null >> $p.new.dat #merge split Vivo(?) sdat
-    sdat2img $p.{transfer.list,new.dat,img} &>/dev/null #convert sdat to img
-    mkdir $p\_ || rm -rf $p/*
-    echo $p 'extracted'
-    sudo mount -t ext4 -o loop $p.img $p\_ &>/dev/null || (mv $p.img temp.img && simg2img temp.img $p.img && rm temp.img && sudo mount -t ext4 -o loop $p.img $p\_ &>/dev/null)
-    sudo chown $(whoami) $p\_/ -R
-    sudo chmod -R u+rwX $p\_/
-done
-mkdir modem_
-for modem in {firmware-update/,}{modem.img,NON-HLOS.bin}; do
-    sudo mount -t vfat -o loop $modem modem_/ && break
-done
 
 if [[ ! -d "${HOME}/extract-dtb" ]]; then
-    cd
-    git clone https://github.com/PabloCastellano/extract-dtb
-    cd -
+    git clone https://github.com/PabloCastellano/extract-dtb ~/extract-dtb
+else
+    git -C ~/extract-dtb pl
 fi
-python3 ~/extract-dtb/extract-dtb.py ./boot.img -o ./bootimg > /dev/null # Extract boot
-(python3 ~/extract-dtb/extract-dtb.py ./dtbo.img -o ./dtbo || python3 ~/extract-dtb/extract-dtb.py ./firmware-update/dtbo.img -o ./dtbo) > /dev/null # Extract dtbo
-echo 'boot extracted'
-for p in system vendor modem cust odm oem; do
-        sudo cp -r $p\_ $p/ #copy images
-        echo $p 'copied'
-        sudo umount $p\_ &>/dev/null #unmount
-        rm -rf $p\_
-done
-#copy file names
-sudo chown $(whoami) * -R ; chmod -R u+rwX * #ensure final permissions
-find system/ -type f -exec echo {} >> allfiles.txt \;
-find vendor/ -type f -exec echo {} >> allfiles.txt \;
-find bootimg/ -type f -exec echo {} >> allfiles.txt \;
-find dtbo/ -type f -exec echo {} >> allfiles.txt \;
-find modem/ -type f -exec echo {} >> allfiles.txt \;
-find cust/ -type f -exec echo {} >> allfiles.txt \;
-find odm/ -type f -exec echo {} >> allfiles.txt \;
-find oem/ -type f -exec echo {} >> allfiles.txt \;
-sort allfiles.txt > all_files.txt
-rm allfiles.txt
-rm *.dat *.list *.br system.img vendor.img 2>/dev/null #remove all compressed files
 
-flavor=$(grep -oP "(?<=^ro.build.flavor=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${flavor}" ]] && flavor=$(grep -oP "(?<=^ro.vendor.build.flavor=).*" -hs vendor/build.prop)
-[[ -z "${flavor}" ]] && flavor=$(grep -oP "(?<=^ro.system.build.flavor=).*" -hs {system,system/system}/build.prop)
-release=$(grep -oP "(?<=^ro.build.version.release=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${release}" ]] && release=$(grep -oP "(?<=^ro.vendor.build.version.release=).*" -hs vendor/build.prop)
-[[ -z "${release}" ]] && release=$(grep -oP "(?<=^ro.system.build.version.release=).*" -hs {system,system/system}/build.prop)
-id=$(grep -oP "(?<=^ro.build.id=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${id}" ]] && id=$(grep -oP "(?<=^ro.vendor.build.id=).*" -hs vendor/build.prop)
-[[ -z "${id}" ]] && id=$(grep -oP "(?<=^ro.system.build.id=).*" -hs {system,system/system}/build.prop)
-incremental=$(grep -oP "(?<=^ro.build.version.incremental=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${incremental}" ]] && incremental=$(grep -oP "(?<=^ro.vendor.build.version.incremental=).*" -hs vendor/build.prop)
-[[ -z "${incremental}" ]] && incremental=$(grep -oP "(?<=^ro.system.build.version.incremental=).*" -hs {system,system/system}/build.prop)
-tags=$(grep -oP "(?<=^ro.build.tags=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${tags}" ]] && tags=$(grep -oP "(?<=^ro.vendor.build.tags=).*" -hs vendor/build.prop)
-[[ -z "${tags}" ]] && tags=$(grep -oP "(?<=^ro.system.build.tags=).*" -hs {system,system/system}/build.prop)
-fingerprint=$(grep -oP "(?<=^ro.build.fingerprint=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${fingerprint}" ]] && fingerprint=$(grep -oP "(?<=^ro.vendor.build.fingerprint=).*" -hs vendor/build.prop)
-[[ -z "${fingerprint}" ]] && fingerprint=$(grep -oP "(?<=^ro.system.build.fingerprint=).*" -hs {system,system/system}/build.prop)
-brand=$(grep -oP "(?<=^ro.product.brand=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${brand}" ]] && brand=$(grep -oP "(?<=^ro.product.vendor.brand=).*" -hs vendor/build.prop)
-[[ -z "${brand}" ]] && brand=$(grep -oP "(?<=^ro.product.system.brand=).*" -hs {system,system/system}/build.prop)
-[[ -z "${brand}" ]] && brand=$(echo $fingerprint | cut -d / -f1 )
-codename=$(grep -oP "(?<=^ro.product.device=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${codename}" ]] && codename=$(grep -oP "(?<=^ro.product.vendor.device=).*" -hs vendor/build.prop)
-[[ -z "${codename}" ]] && codename=$(grep -oP "(?<=^ro.product.system.device=).*" -hs {system,system/system}/build.prop)
-[[ -z "${codename}" ]] && codename=$(echo $fingerprint | cut -d / -f3 | cut -d : -f1 )
-description=$(grep -oP "(?<=^ro.build.description=).*" -hs {system,system/system,vendor}/build.prop)
-[[ -z "${description}" ]] && description=$(grep -oP "(?<=^ro.vendor.build.description=).*" -hs vendor/build.prop)
-[[ -z "${description}" ]] && description=$(grep -oP "(?<=^ro.system.build.description=).*" -hs {system,system/system}/build.prop)
+if [[ ! -d "${HOME}/Firmware-extractor" ]]; then
+    git clone https://github.com/AndroidDumps/Firmware_extractor --recurse-submodules ~/Firmware_extractor
+else
+    git -C ~/Firmware_extractor pl --recurse-submodules
+fi
+
+bash ~/Firmware_extractor/extractor.sh "${FILE}" "${PWD}"
+
+python3 ~/extract-dtb/extract-dtb.py ./boot.img -o ./bootimg > /dev/null
+mkdir bootdts dtbodts
+find bootimg/ -name '*.dtb' -type f -exec dtc -I dtb -O dts {} -o bootdts/"$(echo {} | sed 's/\.dtb/.dts/')" \; > /dev/null
+[[ -f "dtbo.img" ]] && python3 ~/extract-dtb/extract-dtb.py ./dtbo.img -o ./dtbo > /dev/null
+find dtbo/ -name '*.dtb' -type f -exec dtc -I dtb -O dts {} -o dtbodts/"$(echo {} | sed 's/\.dtb/.dts/')" \; > /dev/null
+
+for p in $PARTITIONS; do
+    if [ -f "$p.img" ]; then
+        mkdir "$p" || rm -rf "$p"/*
+        7z x "$p".img -y -o"$p"/
+        rm "$p".img
+    fi
+done
+
+ls system/build*.prop 2>/dev/null || ls system/system/build*.prop 2>/dev/null || ( sendTG "No system build*.prop found, pushing cancelled!" && exit 1 )
+
+
+# board-info.txt
+find ./modem -type f -exec strings {} \; | grep "QC_IMAGE_VERSION_STRING=MPSS." | sed "s|QC_IMAGE_VERSION_STRING=MPSS.||g" | cut -c 4- | sed -e 's/^/require version-baseband=/' >> ./board-info.txt
+find ./tz* -type f -exec strings {} \; | grep "QC_IMAGE_VERSION_STRING" | sed "s|QC_IMAGE_VERSION_STRING|require version-trustzone|g" >> ./board-info.txt
+if [ -f ./vendor/build.prop ]; then
+	strings ./vendor/build.prop | grep "ro.vendor.build.date.utc" | sed "s|ro.vendor.build.date.utc|require version-vendor|g" >> ./board-info.txt
+fi
+sort -u -o ./board-info.txt ./board-info.txt
+
+# Fix permissions
+chown $(whoami) * -R
+chmod -R u+rwX *
+
+# Generate all_files.txt
+find . -type f -printf '%P\n' | sort | grep -v ".git/" > ./all_files.txt
+
+flavor=$(grep -oP "(?<=^ro.build.flavor=).*" -hs {system,system/system,vendor}/build*.prop)
+[[ -z "${flavor}" ]] && flavor=$(grep -oP "(?<=^ro.vendor.build.flavor=).*" -hs vendor/build*.prop)
+[[ -z "${flavor}" ]] && flavor=$(grep -oP "(?<=^ro.system.build.flavor=).*" -hs {system,system/system}/build*.prop)
+[[ -z "${flavor}" ]] && flavor=$(grep -oP "(?<=^ro.build.type=).*" -hs {system,system/system}/build*.prop)
+release=$(grep -oP "(?<=^ro.build.version.release=).*" -hs {system,system/system,vendor}/build*.prop)
+[[ -z "${release}" ]] && release=$(grep -oP "(?<=^ro.vendor.build.version.release=).*" -hs vendor/build*.prop)
+[[ -z "${release}" ]] && release=$(grep -oP "(?<=^ro.system.build.version.release=).*" -hs {system,system/system}/build*.prop)
+id=$(grep -oP "(?<=^ro.build.id=).*" -hs {system,system/system,vendor}/build*.prop)
+[[ -z "${id}" ]] && id=$(grep -oP "(?<=^ro.vendor.build.id=).*" -hs vendor/build*.prop)
+[[ -z "${id}" ]] && id=$(grep -oP "(?<=^ro.system.build.id=).*" -hs {system,system/system}/build*.prop)
+incremental=$(grep -oP "(?<=^ro.build.version.incremental=).*" -hs {system,system/system,vendor}/build*.prop)
+[[ -z "${incremental}" ]] && incremental=$(grep -oP "(?<=^ro.vendor.build.version.incremental=).*" -hs vendor/build*.prop)
+[[ -z "${incremental}" ]] && incremental=$(grep -oP "(?<=^ro.system.build.version.incremental=).*" -hs {system,system/system}/build*.prop)
+tags=$(grep -oP "(?<=^ro.build.tags=).*" -hs {system,system/system,vendor}/build*.prop)
+[[ -z "${tags}" ]] && tags=$(grep -oP "(?<=^ro.vendor.build.tags=).*" -hs vendor/build*.prop)
+[[ -z "${tags}" ]] && tags=$(grep -oP "(?<=^ro.system.build.tags=).*" -hs {system,system/system}/build*.prop)
+fingerprint=$(grep -oP "(?<=^ro.build.fingerprint=).*" -hs {system,system/system,vendor}/build*.prop)
+[[ -z "${fingerprint}" ]] && fingerprint=$(grep -oP "(?<=^ro.vendor.build.fingerprint=).*" -hs vendor/build*.prop)
+[[ -z "${fingerprint}" ]] && fingerprint=$(grep -oP "(?<=^ro.system.build.fingerprint=).*" -hs {system,system/system}/build*.prop)
+brand=$(grep -oP "(?<=^ro.product.brand=).*" -hs {system,system/system,vendor}/build*.prop | head -1)
+[[ -z "${brand}" ]] && brand=$(grep -oP "(?<=^ro.product.vendor.brand=).*" -hs vendor/build*.prop | head -1)
+[[ -z "${brand}" ]] && brand=$(grep -oP "(?<=^ro.vendor.product.brand=).*" -hs vendor/build*.prop | head -1)
+[[ -z "${brand}" ]] && brand=$(grep -oP "(?<=^ro.product.system.brand=).*" -hs {system,system/system}/build*.prop | head -1)
+[[ -z "${brand}" ]] && brand=$(echo "$fingerprint" | cut -d / -f1 )
+codename=$(grep -oP "(?<=^ro.product.device=).*" -hs {system,system/system,vendor}/build*.prop | head -1)
+[[ -z "${codename}" ]] && codename=$(grep -oP "(?<=^ro.product.vendor.device=).*" -hs vendor/build*.prop | head -1)
+[[ -z "${codename}" ]] && codename=$(grep -oP "(?<=^ro.vendor.product.device=).*" -hs vendor/build*.prop | head -1)
+[[ -z "${codename}" ]] && codename=$(grep -oP "(?<=^ro.product.system.device=).*" -hs {system,system/system}/build*.prop | head -1)
+[[ -z "${codename}" ]] && codename=$(echo "$fingerprint" | cut -d / -f3 | cut -d : -f1 )
+[[ -z "${codename}" ]] && codename=$(grep -oP "(?<=^ro.build.fota.version=).*" -hs {system,system/system}/build*.prop | cut -d - -f1 | head -1)
+description=$(grep -oP "(?<=^ro.build.description=).*" -hs {system,system/system,vendor}/build*.prop)
+[[ -z "${description}" ]] && description=$(grep -oP "(?<=^ro.vendor.build.description=).*" -hs vendor/build*.prop)
+[[ -z "${description}" ]] && description=$(grep -oP "(?<=^ro.system.build.description=).*" -hs {system,system/system}/build*.prop)
 [[ -z "${description}" ]] && description="$flavor $release $id $incremental $tags"
-branch=$(echo $description | tr ' ' '-')
-repo=$(echo $brand\_$codename\_dump | tr '[:upper:]' '[:lower:]')
+branch=$(echo "$description" | tr ' ' '-')
+repo=$(echo "$brand"\_"$codename"\_dump | tr '[:upper:]' '[:lower:]')
+
+printf "\nflavor: $flavor\nrelease: $release\nid: $id\nincremental: $incremental\ntags: $tags\nfingerprint: $fingerprint\nbrand: $brand\ncodename: $codename\ndescription: $description\nbranch: $branch\nrepo: $repo\n"
 
 git init
 git config user.name "Akhil's Lazy Buildbot"
@@ -150,3 +134,19 @@ git add system/ ;
 git commit -asm "Add system for ${description}" ;
 git push ssh://git@github.com/$ORG/${repo,,}.git $branch ;) || (sendTG "Pushing failed" && exit 1)
 sendTG "Pushed <a href=\"https://github.com/$ORG/$repo\">$description</a>"
+
+commit_head=$(git log -1 --format=%H)
+commit_link="https://github.com/$ORG/$repo/commit/$commit_head"
+echo -e "Sending telegram notification"
+(
+printf "<b>Brand: $brand</b>"
+printf "\n<b>Device: $codename</b>"
+printf "\n<b>Version:</b> $release"
+printf "\n<b>Fingerprint:</b> $fingerprint"
+printf "\n<b>GitHub:</b>"
+printf "\n<a href=\"$commit_link\">Commit</a>"
+printf "\n<a href=\"https://github.com/$ORG/$repo/tree/$branch/\">$codename</a>"
+) >> tg.html
+TEXT=$(cat tg.html)
+curl -s "https://api.telegram.org/bot${API_KEY}/sendmessage" --data "text=${TEXT}&chat_id=@android_dumps&parse_mode=HTML&disable_web_page_preview=True" > /dev/null
+rm -fv tg.html
