d5a960c5c0d69d5e331d9cb63f0cb70e333de0a3
anirudhgupta109 <anirudhgupta109@gmail.com>
2019-09-01 23:51:08 +0530

    setup: ccache: Track ccache from upstream (#95)
    Also includes tracking the necessary libs from the source instead of Ubuntu's outdated repos
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- setup/ccache.sh
+++ setup/ccache.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 cd /tmp || exit 1
-git clone git://github.com/akhilnarang/ccache.git
+git clone git://github.com/ccache/ccache.git
 cd ccache || exit 1
 ./autogen.sh
-./configure --disable-man
+./configure --disable-man --with-libzstd-from-internet --with-libb2-from-internet
 make -j"$(nproc)"
 sudo make install
 rm -rf "${PWD}"
