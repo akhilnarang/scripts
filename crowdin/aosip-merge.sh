de8e761aa43b356a8c967f96b32e39fb47533541
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-02 15:16:55 +0530

    crowdin/aosip-merge.sh: Shellcheck cleanup
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- crowdin/aosip-merge.sh
+++ crowdin/aosip-merge.sh
@@ -7,9 +7,9 @@
 GERRIT_HOST="review.aosiprom.com"
 QUERY="topic:translations status:open"
 PORT="29418"
-COMMITS=$(ssh -p${PORT} ${GERRIT_HOST} gerrit query ${QUERY} | grep number \
-		| cut -d: -f2)
+COMMITS=$(ssh -p${PORT} ${GERRIT_HOST} gerrit query "${QUERY}" | grep number |
+	cut -d: -f2)
 for COMMIT in ${COMMITS}; do
-	ssh -p29418 ${GERRIT_HOST} gerrit review ${COMMIT},1 --code-review=+2 \
-  		--verified=+1 --submit
+	ssh -p29418 ${GERRIT_HOST} gerrit review "${COMMIT}",1 --code-review=+2 \
+		--verified=+1 --submit
 done
