3e528e1428f5825bfc3f275169f41883eda1fdcd
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-02 15:36:46 +0530

    push/aosip.sh: Shellcheck cleanup
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- push/aosip.sh
+++ push/aosip.sh
@@ -12,18 +12,18 @@ fi
 export AOSIP_SOURCE_DIR="/home/akhil/${BRANCH}"
 export DIR=$PWD
 
-cd ${AOSIP_SOURCE_DIR}
+cd "${AOSIP_SOURCE_DIR}" || exit 1
 . build/envsetup.sh
 
 PROJECTS="$(grep aosip .repo/manifests/snippets/aosip.xml | grep project | awk '{print $2}' | awk -F'"' '{print $2}')"
 
 
 for project in ${PROJECTS}; do
-cd $project
+cd "$project" || continue
 #git push $(git remote -v | head -1 | awk '{print $2}' | sed -e 's/https:\/\/github.com\/AOSiP/ssh:\/\/akhil@review.aosiprom.com:29418\/AOSIP/') HEAD:oreo
 gerrit
-git push gerrit HEAD:refs/heads/${BRANCH}
-cd -
+git push gerrit HEAD:refs/heads/"${BRANCH}"
+cd - || exit 1
 done
 
-cd ${DIR}
+cd "${DIR}" || exit 1
