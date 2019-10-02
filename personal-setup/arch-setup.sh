cb36b47c1552b167f4ef4e91da18d2d06f86723a
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-02 15:51:22 +0530

    personal-setup/{arch,deb}-setup.sh: Append not overwrite
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- personal-setup/arch-setup.sh
+++ personal-setup/arch-setup.sh
@@ -3,7 +3,7 @@
 git config --global user.name "Akhil Narang"
 git config --global user.email "akhilnarang.1999@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
-printf '\n' | tee ~/.bashrc
+printf '\n' | tee -a ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
 echo "onLogin" >> ~/.bashrc
 yaourt -S figlet fortune-mod hub byobu --noconfirm
--- personal-setup/deb-setup.sh
+++ personal-setup/deb-setup.sh
@@ -4,7 +4,7 @@ source ~/scripts/setup/hub.sh
 git config --global user.name "Akhil Narang"
 git config --global user.email "akhilnarang.1999@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
-printf '\n' | tee ~/.bashrc
+printf '\n' | tee -a ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
 echo "onLogin" >> ~/.bashrc
 sudo apt install figlet fortune byobu mosh -y
