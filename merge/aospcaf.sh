906a45a4a6f84744a1acd887b9e0a33bb7da0c24
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-02 16:13:52 +0530

    merge/aospcaf.sh: Shellcheck cleanup
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- merge/aospcaf.sh
+++ merge/aospcaf.sh
@@ -21,23 +21,24 @@ export CAF="https://source.codeaurora.org"
 do_not_merge="device/qcom/common external/ant-wireless/antradio-library external/bash external/busybox external/connectivity external/exfat external/fuse external/ntfs-3g hardware/qcom/bt hardware/qcom/bt-caf hardware/qcom/keymaster hardware/qcom/wlan hardware/qcom/wlan-caf hardware/ril hardware/ril-caf vendor/aosp vendor/qcom/opensource/softap"
 
 # AOSP-CAF manifest is setup with repo name first, then repo path, so the path attribute is after 3 spaces, and the path itself within "" in it
-repos="$(grep 'remote="aosp-caf"' ${AOSPCAF_PATH}/.repo/manifests/manifests/caf.xml  | awk '{print $3}' | awk -F '"' '{print $2}')"
+repos="$(grep 'remote="aosp-caf"' "${AOSPCAF_PATH}"/.repo/manifests/manifests/caf.xml  | awk '{print $3}' | awk -F '"' '{print $2}')"
 
-cd ${AOSPCAF_PATH}
+cd "${AOSPCAF_PATH}" || exit 1
 
 for filess in failed success notcaf; do
 	rm $filess 2> /dev/null
 	touch $filess
 done
 
-for REPO in ${repos}; do
+while read -r REPO; do
 	echo -e ""
+	# shellcheck disable=SC2076,SC2154
 	if [[ "${do_not_merge}" =~ "${REPO}" ]]; then
 		echo -e "${REPO} is not to be merged"
 	else
 		echo "$blu Merging $REPO $end"
 		echo -e ""
-		cd $REPO
+		cd "$REPO" || continue
 		git checkout n-mr2
 		git fetch aosp-caf n-mr2
 		git reset --hard aosp-caf/n-mr2
@@ -45,22 +46,21 @@ for REPO in ${repos}; do
 		if [[ "${REPO}" == "device/qcom/sepolicy" ]]; then
 			reponame="${CAF}/${REPO}"
 		elif [[ "${REPO}" =~ "vendor/qcom" ]]; then
-			reponame="${CAF}/platform/$(echo ${REPO} | sed -e 's|qcom/opensource|qcom-opensource|')"
+			# shellcheck disable=SC2001
+			reponame="${CAF}/platform/$(echo "${REPO}" | sed -e 's|qcom/opensource|qcom-opensource|')"
 		else
 			reponame="${CAF}/platform/$REPO"
 		fi
 		git remote add caf "${reponame}"
-		git fetch caf --quiet --tags
-		if [ $? -ne 0 ]; then
-			echo "$repos" >> ${AOSPCAF_PATH}/notcaf
+		if ! git fetch caf --quiet --tags; then
+			echo "$repos" >> "${AOSPCAF_PATH}"/notcaf
 		else
-			git merge ${TAG} --no-edit
-			if [ $? -ne 0 ]; then
-				echo "$REPO" >> ${AOSPCAF_PATH}/failed
+			if ! git merge "${TAG}" --no-edit; then
+				echo "$REPO" >> "${AOSPCAF_PATH}"/failed
 				echo "$red $REPO failed :( $end"
 			else
 				if [[ "$(git rev-parse HEAD)" != "$(git rev-parse aosp-caf/n-mr2)" ]]; then
-					echo "$REPO" >> ${AOSPCAF_PATH}/success
+					echo "$REPO" >> "${AOSPCAF_PATH}"/success
 					echo "$grn $REPO succeeded $end"
 				else
 					echo "$REPO - unchanged"
@@ -68,9 +68,9 @@ for REPO in ${repos}; do
 			fi
 			echo -e ""
 		fi
-			cd ${AOSPCAF_PATH}
+        cd "${AOSPCAF_PATH}" || exit 1
 	fi
-done
+done < <(echo "${repos}")
 
 echo -e ""
 echo -e "$red These REPO failed $end"
