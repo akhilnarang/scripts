b6787cdd7342b31053b0d74c0462bae7893cf41c
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-02 15:33:39 +0530

    personal-setup/arch-setup.sh: Shellcheck cleanup
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- personal-setup/arch-setup.sh
+++ personal-setup/arch-setup.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env sh
+
 git config --global user.name "Akhil Narang"
 git config --global user.email "akhilnarang.1999@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
-echo "" >> ~/.bashrc
+printf '\n' | tee ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
 echo "onLogin" >> ~/.bashrc
 yaourt -S figlet fortune-mod hub byobu --noconfirm
-
