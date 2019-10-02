89c823849c23cfe304837d614b46ef19cd46817b
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-02 15:34:34 +0530

    personal-setup/deb-setup.sh: Shellcheck cleanup
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- personal-setup/deb-setup.sh
+++ personal-setup/deb-setup.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
+
 source ~/scripts/setup/hub.sh
 git config --global user.name "Akhil Narang"
 git config --global user.email "akhilnarang.1999@gmail.com"
 git config --global credential.helper "cache --timeout=7200"
-echo "" >> ~/.bashrc
+printf '\n' | tee ~/.bashrc
 echo "source ~/scripts/functions" >> ~/.bashrc
 echo "onLogin" >> ~/.bashrc
 sudo apt install figlet fortune byobu mosh -y
-
