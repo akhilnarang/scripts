c74b43166c12c858835631f3408041deda015776
Akhil Narang <akhilnarang.1999@gmail.com>
2019-10-04 17:26:08 +0530

    ninja: use Harsh's fork
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/ninja.sh
+++ setup/ninja.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /tmp || exit 1
-git clone git://github.com/akhilnarang/ninja.git
+git clone git://github.com/msfjarvis/ninja.git
 cd ninja || exit 1
 ./configure.py --bootstrap
 sudo install ./ninja /usr/local/bin/ninja
