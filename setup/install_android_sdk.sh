8b0c70df9198cea45c5d18065ebc754d83c08a42
Harsh Shandilya <msfjarvis@gmail.com>
2019-10-11 15:17:33 +0530

    setup: Rewrite SDK installer
    - Set both ANDROID_HOME and ANDROID_SDK_ROOT
    - Download to tmpfs and extract from there
    - Don't enforce use of `source`, not required anymore.
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- setup/install_android_sdk.sh
+++ setup/install_android_sdk.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 
-if [ "${BASH_SOURCE[0]}" -ef "$0" ]
-then
-    echo "This script relies on information from your environment and thus should be sourced."
-    exit 1
-fi
-
 # Script to setup the Android SDK on a Linux System
 CUR_DIR="$(cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd)"
 CUR_DIR="${CUR_DIR/setup/}"
+SDK_DIR="${HOME}/Android/Sdk"
 TOOLS_URL="https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
-ZIP_NAME=$(printf '%s\n' "${TOOLS_URL##*/}")
-mkdir -p ~/Android/Sdk/; cd ~/Android/Sdk || exit 1
-if axel -a -n 10 "${TOOLS_URL}" || wget "${TOOLS_URL}" ; then
-    unzip -o "${ZIP_NAME}"
+ZIP_NAME=sdk.zip
+mkdir -p "${SDK_DIR}"/
+cd /tmp/ || exit 1
+if axel -a -n 10 "${TOOLS_URL}" -o "${ZIP_NAME}"  || wget "${TOOLS_URL}" -O "${ZIP_NAME}"; then
+    unzip -o "${ZIP_NAME}" -d "${SDK_DIR}" && rm "${ZIP_NAME}"
 else
     exit 1
 fi
+
 # Create repositories.cfg if not present
 if [ ! -f ~/.android/repositories.cfg ] ; then
     touch ~/.android/repositories.cfg
 fi
-rm "${ZIP_NAME}"
-printf '\nexport ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
-source ~/.bashrc
-yes | "${ANDROID_HOME}"/tools/bin/sdkmanager --licenses
+
+if [ -z "${ANDROID_HOME}" ]; then
+    printf "\nexport ANDROID_HOME=%s" "${SDK_DIR}" >> ~/.bashrc
+fi
+if [ -z "${ANDROID_SDK_ROOT}" ]; then
+    printf "\nANDROID_SDK_ROOT=%s" "${SDK_DIR}" >> ~/.bashrc
+fi
+
+yes | "${SDK_DIR}"/tools/bin/sdkmanager --licenses
 
 while read -r p; do
-    "${ANDROID_HOME}"/tools/bin/sdkmanager "${p}"
+    "${SDK_DIR}"/tools/bin/sdkmanager "${p}"
 done < "${CUR_DIR}/setup"/android-sdk-minimal.txt
 
 cd "${CUR_DIR}" || exit
