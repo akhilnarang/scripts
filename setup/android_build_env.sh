af0ae08e3e6434e861ef72dc2b020eb962fcf644
Kshitij Gupta <kshitijgm@gmail.com>
2019-10-24 11:55:18 +0530

    android_env_setup: Also install libisl15 (#107)
    - Some TCs need libisl.so.15 which is provided by package libisl15
    
    Change-Id: I65b3b78c1525d94989d99406d6397f59ae82032e
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -29,7 +29,7 @@ fi
 sudo apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt install -y adb autoconf automake axel bc bison build-essential ccache clang cmake expat fastboot flex \
     g++ g++-multilib gawk gcc gcc-multilib git-core gnupg gperf htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 \
-    libc6-dev libcap-dev libexpat1-dev libgmp-dev liblz4-* liblzma* libmpc-dev libmpfr-dev \
+    libc6-dev libcap-dev libexpat1-dev libgmp-dev libisl15 liblz4-* liblzma* libmpc-dev libmpfr-dev \
     libncurses5-dev libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils lzma* lzop maven ncftp ncurses-dev \
     patch patchelf pkg-config pngcrush pngquant python python-all-dev re2c schedtool squashfs-tools subversion texinfo \
     unzip w3m xsltproc zip zlib1g-dev lzip "${PACKAGES}"
