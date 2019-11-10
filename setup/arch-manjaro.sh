292cd2838af8bc0322458d57cc26db2940eb9c82
baalajimaestro <baalajimaestro@pixelexperience.org>
2019-11-10 13:10:58 +0530

    scripts: arch: Ask for git username while installing
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

--- setup/arch-manjaro.sh
+++ setup/arch-manjaro.sh
@@ -45,6 +45,24 @@ sudo make install
 cd ..
 rm -rf ccache-3.6 ccache-3.6.tar.xz
 
+# For all those distro hoppers, lets setup your git credentials
+GIT_USERNAME="$(git config --get user.name)"
+GIT_EMAIL="$(git config --get user.email)"
+echo "Configuring Git"
+if [[ -z ${GIT_USERNAME} ]]; then
+	echo "Enter your name: "
+	read -r NAME
+	git config --global user.name "${NAME}"
+fi
+if [[ -z ${GIT_EMAIL} ]]; then
+	echo "Enter your email: "
+	read -r EMAIL
+	git config --global user.email "${EMAIL}"
+fi
+git config --global credential.helper "cache --timeout=7200"
+echo "Github credentials setup successfully"
+
+
 echo "All Done :'D"
 echo "Don't forget to run these commands before building, or make sure the python in your PATH is python2 and not python3"
 echo "
