682140d9e1a5f4af99cd8c4d06a7f43852385c98
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-28 01:27:53 +0530

    scripts: add a script to make a gitlab repository private
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ gitlab/make-project-public.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# visibility can be one of 'public', 'private', or 'internal'
+
+PROJECT="exconfidential%2fsprd%2fbuild"
+
+curl -X PUT "https://gitlab.com/api/v4/projects/${PROJECT}"  -H "Authorization: Bearer ${GITLAB_TOKEN}" -d "{ 'visibility': 'public' }"
+
