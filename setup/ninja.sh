1374ff83e9e611854c72ebb281c7de4af55f3bcc
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-30 22:50:02 +0530

    setup: remove unused scripts
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/ninja.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /tmp || exit 1
-git clone git://github.com/msfjarvis/ninja.git
-cd ninja || exit 1
-./configure.py --bootstrap
-sudo install ./ninja /usr/local/bin/ninja
-rm -rf "${PWD}"
-cd - || exit 1
