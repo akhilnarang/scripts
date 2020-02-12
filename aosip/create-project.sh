bc645401f8c8144c969df4358948df72d07ebd7f
Akhil Narang <akhilnarang.1999@gmail.com>
2020-02-12 20:29:08 +0530

    aosip: create-project: let it work
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip/create-project.sh
+++ aosip/create-project.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 source ~/scripts/functions
 echo "Cloning repository"
-git clone "${REPO_LINK}" -b "${REPO_BRANCH}" --verbose --progress
+git clone "${REPO_LINK}" -b "${REPO_BRANCH}" --verbose --progress "${REPO_NAME}"
+cd "${REPO_NAME}" || exit 1
 ssh -p29418 review.aosip.dev gerrit create-project "${ORG}/${REPO_NAME}" -p All-Projects -t REBASE_IF_NECESSARY
 curl -s -H "Authorization: token ${GITHUB_OAUTH_TOKEN:?}" -d '{ "name": "'"${REPO_NAME}"'" }' "https://api.github.com/orgs/${ORG}/repos"
 git checkout "${REPO_BRANCH}"
