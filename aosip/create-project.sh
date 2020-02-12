985dbef867baae1b10c49319caa31eb26d884239
Akhil Narang <akhilnarang.1999@gmail.com>
2020-02-12 20:52:21 +0530

    create-project: review.aosiprom.com > review.aosip.dev
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip/create-project.sh
+++ aosip/create-project.sh
@@ -6,5 +6,5 @@ cd "${REPO_NAME}" || exit 1
 ssh -p29418 review.aosip.dev gerrit create-project "${ORG}/${REPO_NAME}" -p All-Projects -t REBASE_IF_NECESSARY
 curl -s -H "Authorization: token ${GITHUB_OAUTH_TOKEN:?}" -d '{ "name": "'"${REPO_NAME}"'" }' "https://api.github.com/orgs/${ORG}/repos"
 git checkout "${REPO_BRANCH}"
-git push ssh://review.aosiprom.com:29418/"${ORG}"/"${REPO_NAME}" HEAD:refs/heads/"${BRANCH}" --verbose --progress
+git push ssh://review.aosip.dev:29418/"${ORG}"/"${REPO_NAME}" HEAD:refs/heads/"${BRANCH}" --verbose --progress
 sendAOSiP "Created and pushed \`$ORG/$REPO_NAME\`"
