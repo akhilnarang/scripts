062795992f7655d293dba01946d1034a78a21230
Akhil Narang <akhilnarang.1999@gmail.com>
2020-02-29 18:59:31 +0530

    aosip: crowdin: update for ten and new domain
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip/crowdin.sh
+++ aosip/crowdin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source ~/.rvm/scripts/rvm
-repo init -u git://github.com/AOSiP/platform_manifest.git -b pie -m crowdin.xml --no-tags --current-branch
-time repo sync -j8 --force-sync --force-broken --no-tags --current-branch
-repo forall -j8 -c "scp -p -P 29418 review.aosiprom.com:hooks/commit-msg .git/hooks/"
+repo init -u git://github.com/AOSiP/platform_manifest.git -b ten -m crowdin.xml --no-tags --current-branch
+time repo sync -j"$(nproc)" --force-sync --force-broken --no-tags --current-branch
+repo forall -j"$(nproc)" -c "scp -p -P 29418 review.aosip.dev:hooks/commit-msg .git/hooks/"
 ./crowdin/sync.py "${@}"
