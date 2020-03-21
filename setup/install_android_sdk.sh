01055055f16c09840dd3908f8850bedd6ceda3cb
Harsh Shandilya <me@msfjarvis.dev>
2020-03-21 13:13:01 +0530

    setup: install_android_sdk: update from https://github.com/msfjarvis/dotfiles/blob/master/setup/android-sdk.sh
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- setup/install_android_sdk.sh
+++ setup/install_android_sdk.sh
@@ -1,35 +1,25 @@
 #!/usr/bin/env bash
 
-# Script to setup the Android SDK on a Linux System
+# Copyright (C) Harsh Shandilya <msfjarvis@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-only
+
 CUR_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 CUR_DIR="${CUR_DIR/setup/}"
-SDK_DIR="${HOME}/Android/Sdk"
-TOOLS_URL="https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
-ZIP_NAME=sdk.zip
-mkdir -p "${SDK_DIR}"/
-cd /tmp/ || exit 1
-if axel -a -n 10 "${TOOLS_URL}" -o "${ZIP_NAME}" || wget "${TOOLS_URL}" -O "${ZIP_NAME}"; then
-    unzip -o "${ZIP_NAME}" -d "${SDK_DIR}" && rm "${ZIP_NAME}"
-else
-    exit 1
-fi
+SDK_TOOLS=commandlinetools-linux-6200805_latest.zip
 
-# Create repositories.cfg if not present
-if [ ! -f ~/.android/repositories.cfg ]; then
-    touch ~/.android/repositories.cfg
-fi
+function setup_android_sdk() {
+  echo "Installing Android SDK"
+  mkdir -p "${HOME:?}"/Android/Sdk
+  cd "${HOME}"/Android/Sdk || return 1
+  if [ ! -f "${SDK_TOOLS}" ]; then
+    aria2c https://dl.google.com/android/repository/"${SDK_TOOLS}"
+  fi
+  unzip -qo "${SDK_TOOLS}"
+  while read -r package; do
+    yes | ./tools/bin/sdkmanager --sdk_root="$(pwd)" "${package:?}"
+  done <"${CUR_DIR}"/setup/android-sdk-minimal.txt
+  rm "${SDK_TOOLS}"
+  cd - || exit
+}
 
-if [ -z "${ANDROID_HOME}" ]; then
-    printf "\nexport ANDROID_HOME=%s" "${SDK_DIR}" >>~/.bashrc
-fi
-if [ -z "${ANDROID_SDK_ROOT}" ]; then
-    printf "\nANDROID_SDK_ROOT=%s" "${SDK_DIR}" >>~/.bashrc
-fi
-
-yes | "${SDK_DIR}"/tools/bin/sdkmanager --licenses
-
-while read -r p; do
-    "${SDK_DIR}"/tools/bin/sdkmanager "${p}"
-done <"${CUR_DIR}/setup"/android-sdk-minimal.txt
-
-cd "${CUR_DIR}" || exit
+setup_android_sdk
