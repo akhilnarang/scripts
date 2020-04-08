d22b6ecde74bc3fa739714964bcc071edead3308
Akhil Narang <me@akhilnarang.dev>
2020-04-08 17:54:51 +0530

    I hate this, but it will have to do for now
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- aosip/sign-and-upload.sh
+++ aosip/sign-and-upload.sh
@@ -31,10 +31,12 @@ SIGNED_TARGET_FILES="signed-target-files.zip"
 SIGNING_FLAGS="-e CronetDynamite.apk= -e DynamiteLoader.apk= -e DynamiteModulesA.apk= -e AdsDynamite.apk= -e DynamiteModulesC.apk= -e MapsDynamite.apk= -e GoogleCertificates.apk= -e AndroidPlatformServices.apk="
 # shellcheck disable=SC2086
 # SC2086: Double quote to prevent globbing and word splitting
+cd ~/ten
 echo "Signing target_files APKs"
-~/ten/build/make/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs $SIGNING_FLAGS aosip_"$DEVICE"-target_files-*.zip "$SIGNED_TARGET_FILES"
+./build/make/tools/releasetools/sign_target_files_apks -o -d ~/.android-certs $SIGNING_FLAGS "$OLDPWD"/aosip_"$DEVICE"-target_files-*.zip "$OLDPWD"/"$SIGNED_TARGET_FILES"
 echo "Generating signed otapackage"
-~/ten/build/make/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey --backup=true "$SIGNED_TARGET_FILES" "$SIGNED_OTAPACKAGE"
+./build/make/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey --backup=true "$OLDPWD"/"$SIGNED_TARGET_FILES" "$OLDPWD"/"$SIGNED_OTAPACKAGE"
+cd -
 ~/api/generate_json.py "$SIGNED_OTAPACKAGE" > /var/www/html/"${DEVICE}"-"${AOSIP_BUILDTYPE}".json
 rclone copy -P --drive-chunk-size 256M "$SIGNED_OTAPACKAGE" kronic-sync:jenkins/"$PARAM_BUILD_NUMBER"
 mkdir -pv /var/www/html/"$PARAM_BUILD_NUMBER"
