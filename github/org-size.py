415fa83787131b33cdf7977cb4ffaf098ad0bd5f
Akhil Narang <me@akhilnarang.dev>
2020-04-28 00:08:34 +0530

    scripts: github: org-size: print repo count, add support for oauth token
      * Without a token github api will ratelimit you pretty quick
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- github/org-size.py
+++ github/org-size.py
@@ -1,17 +1,31 @@
+#!/usr/bin/env python3
+
 from humanize import naturalsize
 from requests import get
+from os import getenv
+
+GITHUB_OAUTH_TOKEN = getenv("GITHUB_OAUTH_TOKEN")
+
+if GITHUB_OAUTH_TOKEN is None:
+    GITHUB_OAUTH_TOKEN = input(
+        "Enter your Github API token or try without one, which may result in you getting rate-limited soon: "
+    )
+
+headers = {"Authorization": "token " + GITHUB_OAUTH_TOKEN}
 
 org = input("Enter organization name: ")
 
 org_size = []
-response = get(f"https://api.github.com/orgs/{org}/repos")
+repo_count = 0
+response = get(f"https://api.github.com/orgs/{org}/repos", headers=headers)
 while True:
     for repo in response.json():
         org_size.append(int(repo["size"]))
+        repo_count += 1
     try:
-        response = get(response.links["next"]["url"])
+        response = get(response.links["next"]["url"], headers=headers)
     except KeyError:
         break
 
 total = sum(org_size * 1024)
-print(naturalsize(total))
+print(f'{org} has {repo_count} repositories which sum up to {naturalsize(total)}')
