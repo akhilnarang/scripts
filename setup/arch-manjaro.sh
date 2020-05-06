f97497dec9ceb46c5139862fc4878a4739c897d5
merothh <merothh@gmail.com>
2020-05-07 01:35:48 +0530

    arch-manjaro.sh: use the android-udev package instead
    * https://www.archlinux.org/packages/community/any/android-udev/

--- setup/arch-manjaro.sh
+++ setup/arch-manjaro.sh
@@ -19,12 +19,8 @@ for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 l
     rm -rf "${package}"
 done
 
-sudo pacman -S android-tools
-echo -e "Setting up udev rules for adb!"
-sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
-sudo chmod 644 /etc/udev/rules.d/51-android.rules
-sudo chown root /etc/udev/rules.d/51-android.rules
-sudo udevadm control --reload-rules
+echo -e "Installing platform tools & udev rules for adb!"
+sudo pacman -S android-tools android-udev
 
 echo "All Done :'D"
 echo "Don't forget to run these commands before building, or make sure the python in your PATH is python2 and not python3"
