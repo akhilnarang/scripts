991c7023eb0a4723a304d00dd0ee498807f1577b
Mandeep Taneja <mandeeptaneja21@outlook.com>
2020-06-03 17:23:03 +0530

    Caring for my lazy self

---
+++ setup/cloner.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/MandeepTaneja/device_xiaomi_beryllium.git -b ten device/xiaomi/beryllium
+git clone https://github.com/MandeepTaneja/device_xiaomi_sdm845-common.git device/xiaomi/sdm845-common
+git clone https://github.com/MandeepTaneja/kernel_xiaomi_beryllium.git kernel/xiaomi/beryllium
+git clone https://github.com/MandeepTaneja/vendor_xiaomi.git vendor/xiaomi 
+git clone https://github.com/MandeepTaneja/android_hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/MandeepTaneja/lolz_clang.git  prebuilts/clang/host/linux-x86/clang-11
+git clone https://gitlab.com/PixelExperience/vendor_xiaomi-firmware.git vendor/xiaomi-firmware
