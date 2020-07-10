f4a7c09acae35d227979902eccafceb4f9b94a27
Akhil Narang <me@akhilnarang.dev>
2020-07-10 14:42:05 +0530

    extract_and_push: make repo public while setting default branch
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- misc/extract_and_push.sh
+++ misc/extract_and_push.sh
@@ -257,7 +257,7 @@ curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4
 project_id="$(jq .id x)"
 rm -f x
 if [[ -z "$project_id" ]]; then
-    curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects" -X POST -F namespace_id="$group_id" -F name="$repo" -F visibility=public > x
+    curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects" -X POST -F namespace_id="$group_id" -F name="$repo" > x
     project_id="$(jq .id x)"
     rm -f x
     if [[ -z "$project_id" ]]; then
@@ -290,7 +290,7 @@ git push "https://dumper:$DUMPER_TOKEN@git.rip/$ORG/$repo.git" HEAD:refs/heads/"
 }
    
 # Set default branch to the newly pushed branch
-curl -s -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects/$project_id" -X PUT -F default_branch="$branch" > /dev/null
+curl -s -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects/$project_id" -X PUT -F default_branch="$branch" -F visibility=public > /dev/null
 
 # Send message to Telegram group
 sendTG "Pushed <a href=\"https://git.rip/$ORG/$repo\">$description</a>"
