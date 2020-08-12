40d7ad5e559bb53825a3fb1dc3eccd75666ca1cd
Akhil Narang <me@akhilnarang.dev>
2020-08-12 17:17:15 +0530

    extract_and_push: some debugging
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- misc/extract_and_push.sh
+++ misc/extract_and_push.sh
@@ -263,11 +263,15 @@ rm -f x
 
 # Create the repo if it doesn't exist
 curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects/$ORG%2f$repo_subgroup%2f$repo_name" > x
+echo "Output of existence x"
+jq . x
 message="$(jq -r .message x)"
 project_id="$(jq .id x)"
 rm -f x
 if [[ $message == "404 Project Not Found" ]]; then
     curl --silent -H "Authorization: bearer ${DUMPER_TOKEN}" "https://git.rip/api/v4/projects" -X POST -F namespace_id="$group_id" -F name="$repo" -F visibility=public > x
+    echo "Output of project creation"
+    jq . x
     project_id="$(jq .id x)"
     rm -f x
     if [[ -z $project_id ]]; then
