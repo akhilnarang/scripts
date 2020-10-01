0c94d1a08b61e29c2a0510c94ffaf963bb50af7d
Akhil Narang <me@akhilnarang.dev>
2020-10-01 13:45:28 +0530

    setup: android_build_env: update some version-specific packages
    Remove libncurses5 symlink where its not required - install it with apt for Debian 10 and Ubuntu 20
    
    Drop support for some versions of Ubuntu since they are EOL
    
    LSB_RELEASE is anyway "Ubuntu 20.04 LTS" now so this wasn't getting applied, and is not needed
    
    Just install python-is-python3 for focal, the python package defaults to python-is-python2
    
    Ubuntu 18.04 seems to have curl
    
    Resolves https://github.com/akhilnarang/scripts/issues/136
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -7,10 +7,9 @@
 # Script to setup an AOSP Build environment on Ubuntu and Linux Mint
 
 LATEST_MAKE_VERSION="4.3"
-UBUNTU_14_PACKAGES="binutils-static curl figlet libesd0-dev libwxgtk2.8-dev schedtool"
 UBUNTU_16_PACKAGES="libesd0-dev"
-UBUNTU_18_PACKAGES="curl"
-UBUNTU_20_PACKAGES="python"
+UBUNTU_20_PACKAGES="libncurses5 python-is-python3"
+DEBIAN_10_PACKAGES="libncurses5"
 PACKAGES=""
 
 sudo apt update
@@ -20,14 +19,12 @@ sudo apt install lsb-core -y
 
 LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
 
-if [[ ${LSB_RELEASE} =~ "Ubuntu 14" ]]; then
-    PACKAGES="${UBUNTU_14_PACKAGES}"
-elif [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
+if [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
     PACKAGES="${UBUNTU_16_PACKAGES}"
-elif [[ ${LSB_RELEASE} =~ "Ubuntu 18" || ${LSB_RELEASE} =~ "Ubuntu 19" || ${LSB_RELEASE} =~ "Deepin" ]]; then
-    PACKAGES="${UBUNTU_18_PACKAGES}"
 elif [[ ${LSB_RELEASE} =~ "Ubuntu 20" ]]; then
     PACKAGES="${UBUNTU_20_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
+    PACKAGES="${DEBIAN_10_PACKAGES}"
 fi
 
 sudo DEBIAN_FRONTEND=noninteractive \
@@ -60,14 +57,6 @@ if [[ -z ${GIT_EMAIL} ]]; then
 fi
 git config --global credential.helper "cache --timeout=7200"
 echo "git identity setup successfully!"
-
-# From Ubuntu 18.10 onwards and Debian Buster libncurses5 package is not available, so we need to hack our way by symlinking required library
-# shellcheck disable=SC2076
-if [[ ${LSB_RELEASE} =~ "Ubuntu 18.10" || ${LSB_RELEASE} =~ "Ubuntu 19" || ${LSB_RELEASE} =~ "Ubuntu Focal Fossa" || ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
-    if [[ -e /lib/x86_64-linux-gnu/libncurses.so.6 && ! -e /usr/lib/x86_64-linux-gnu/libncurses.so.5 ]]; then
-        sudo ln -s /lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
-    fi
-fi
 
 if [[ "$(command -v adb)" != "" ]]; then
     echo -e "Setting up udev rules for adb!"
