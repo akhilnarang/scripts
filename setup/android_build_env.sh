782579f9a0f95e8027874d2f97ef5a8f17d17dd2
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-10-10 18:39:21 +0530

    Update android_build_env.sh
    Added curl to Ubuntu 20 Packages...

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -8,7 +8,7 @@
 
 LATEST_MAKE_VERSION="4.3"
 UBUNTU_16_PACKAGES="libesd0-dev"
-UBUNTU_20_PACKAGES="libncurses5 python-is-python3"
+UBUNTU_20_PACKAGES="libncurses5 curl python-is-python3"
 DEBIAN_10_PACKAGES="libncurses5"
 PACKAGES=""
 
@@ -30,7 +30,7 @@ fi
 sudo DEBIAN_FRONTEND=noninteractive \
     apt install \
     adb autoconf automake axel bc bison build-essential \
-    ccache clang cmake curl expat fastboot flex g++ \
+    ccache clang cmake expat fastboot flex g++ \
     g++-multilib gawk gcc gcc-multilib git gnupg gperf \
     htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
     libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
