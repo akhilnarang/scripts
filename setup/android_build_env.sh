066adc901e44656d40e87c5b3fc059956a066780
Vinit Chauhan <chauhanvinit23@gmail.com>
2020-10-10 17:55:34 +0530

    Update android_build_env.sh
    curl was missing for new systems.

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -30,7 +30,7 @@ fi
 sudo DEBIAN_FRONTEND=noninteractive \
     apt install \
     adb autoconf automake axel bc bison build-essential \
-    ccache clang cmake expat fastboot flex g++ \
+    ccache clang cmake curl expat fastboot flex g++ \
     g++-multilib gawk gcc gcc-multilib git gnupg gperf \
     htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
     libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
