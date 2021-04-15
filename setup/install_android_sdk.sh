b8bbe13817b3bd6075648d8d814f1065c9cae9f6
Stefen Wakefield <me@xstefen.dev>
2021-04-15 05:13:58 -0500

    setup/install_android_sdk: update packages and sync with upstream (#160)
    Co-authored-by: Harsh Shandilya <me@msfjarvis.dev>

--- setup/install_android_sdk.sh
+++ setup/install_android_sdk.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-# Copyright (C) Harsh Shandilya <msfjarvis@gmail.com>
+# Copyright (C) Harsh Shandilya <me@msfjarvis.dev>
 # SPDX-License-Identifier: GPL-3.0-only
+
+trap 'rm -rf /tmp/tools.zip 2>/dev/null' INT TERM EXIT
 
 CUR_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 CUR_DIR="${CUR_DIR/setup/}"
-SDK_TOOLS=commandlinetools-linux-6609375_latest.zip
+SDK_TOOLS=commandlinetools-linux-6858069_latest.zip
 
 function setup_android_sdk() {
     echo "Installing Android SDK"
-    mkdir -p "${HOME:?}"/Android/Sdk
-    cd "${HOME}"/Android/Sdk || return 1
+    SDK_DIR="${HOME:?}/Android/Sdk"
+    mkdir -p "${SDK_DIR}"
     if [ ! -f "${SDK_TOOLS}" ]; then
-        aria2c https://dl.google.com/android/repository/"${SDK_TOOLS}"
+        wget https://dl.google.com/android/repository/"${SDK_TOOLS}" -O /tmp/tools.zip
     fi
-    unzip -qo "${SDK_TOOLS}"
+    unzip -qo /tmp/tools.zip -d "${SDK_DIR}"
     while read -r package; do
-        yes | ./tools/bin/sdkmanager --sdk_root="$(pwd)" "${package:?}"
-    done < "${CUR_DIR}"/setup/android-sdk-minimal.txt
-    rm "${SDK_TOOLS}"
+        yes | "${SDK_DIR}"/cmdline-tools/bin/sdkmanager --sdk_root="${SDK_DIR}" "${package:?}"
+    done <"${CUR_DIR}"/setup/android-sdk-minimal.txt
+    rm /tmp/tools.zip
     cd - || exit
 }
 
