a6f2d78e7cb24caf0c1df46c7169cf94040ecc90
Akhil Narang <me@akhilnarang.dev>
2021-05-12 12:00:24 +0530

    Cleanup arch setup a bit
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- setup/arch-manjaro.sh
+++ setup/arch-manjaro.sh
@@ -3,7 +3,7 @@
 # Script to setup an android build environment on Arch Linux and derivative distributions
 
 clear
-echo '-- Starting Arch-based Android build setup'
+echo 'Starting Arch-based Android build setup'
 # Uncomment the multilib repo, incase it was commented out
 echo '[1/4] Enabling multilib repo'
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
@@ -12,17 +12,18 @@ echo '[2/4] Syncing repositories and updating system packages'
 sudo pacman -Syyu --noconfirm --needed multilib-devel
 # Install android build prerequisites
 echo '[3/4] Installing Android building prerequisites'
-pkgz="lineageos-devel"
-for pkg in $pkgz; do
-  git clone https://aur.archlinux.org/"$pkg"
-  cd "$pkg" || exit
-  makepkg -si --skippgpcheck --noconfirm --needed
-  cd ..
-  rm -rf "$pkg"
+packages="lineageos-devel"
+for package in $packages; do
+    echo "Installing $package"
+    git clone https://aur.archlinux.org/"$package"
+    cd "$package" || exit
+    makepkg -si --skippgpcheck --noconfirm --needed
+    cd - || exit
+    rm -rf "$package"
 done
 
 # Install adb and associated udev rules
 echo '[4/4] Installing adb convenience tools'
 sudo pacman -S --noconfirm --needed android-tools android-udev
 
-echo '-- Setup completed, ready for kanging, enjoy'
+echo 'Setup completed, enjoy'
