dd458f5e2dcad1318949b78b05dce822087d3b3e
Akhil Narang <me@akhilnarang.dev>
2021-05-15 19:46:57 +0530

    android_build_env: extend focal packages to hirsute as well
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -25,7 +25,7 @@ LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
 
 if [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
     PACKAGES="${UBUNTU_16_PACKAGES}"
-elif [[ ${LSB_RELEASE} =~ "Ubuntu 20" ]]; then
+elif [[ ${LSB_RELEASE} =~ "Ubuntu 20" || ${LSB_RELEASE} =~ "Ubuntu 21" ]]; then
     PACKAGES="${UBUNTU_20_PACKAGES}"
 elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
     PACKAGES="${DEBIAN_10_PACKAGES}"
