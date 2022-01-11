783bf3d782c31f86d8a2174f85a03b994fe91af7
Kn0ax <42520461+Kn0ax@users.noreply.github.com>
2022-01-11 14:56:20 +0100

    removed -get

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -14,7 +14,7 @@ DEBIAN_11_PACKAGES="libncurses5"
 PACKAGES=""
 
 echo "Adding GitHub apt key and repository!"
-sudo apt-get install software-properties-common -y
+sudo apt install software-properties-common -y
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
 
