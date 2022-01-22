d1184879f471d02b4d7b74a5191a28b257fc85fa
LeCmnGend <46700663+LeCmnGend@users.noreply.github.com>
2022-01-22 18:07:27 +0700

    Delete gitlab directory

--- gitlab/list-repos.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-PROJECT_ID=6775874
-
-curl "https://gitlab.com/api/v4/groups/${PROJECT_ID}/projects" -H "Authorization: Bearer ${GITLAB_TOKEN}" -G -d per_page=100
--- gitlab/make-project-public.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# visibility can be one of 'public', 'private', or 'internal'
-
-PROJECT="exconfidential%2fsprd%2fbuild"
-
-curl -X PUT "https://gitlab.com/api/v4/projects/${PROJECT}" -H "Authorization: Bearer ${GITLAB_TOKEN}" -d "{ 'visibility': 'public' }"
