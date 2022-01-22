85b265c7a439af51ec7c768a92539988e4a42355
LeCmnGend <46700663+LeCmnGend@users.noreply.github.com>
2022-01-22 18:21:45 +0700

    Delete personal-setup directory

--- personal-setup/arch-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-git config --global user.name "Akhil Narang"
-git config --global user.email "akhilnarang.1999@gmail.com"
-git config --global credential.helper "cache --timeout=7200"
-printf '\n' | tee -a ~/.bashrc
-echo "source ~/scripts/functions" >> ~/.bashrc
-echo "onLogin" >> ~/.bashrc
-yaourt -S figlet fortune-mod hub byobu --noconfirm
