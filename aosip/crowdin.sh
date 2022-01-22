9d2de358ae4e0802b8493907f844ac89f13448c2
LeCmnGend <46700663+LeCmnGend@users.noreply.github.com>
2022-01-22 18:06:46 +0700

    Delete aosip directory

--- aosip/crowdin.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source ~/.rvm/scripts/rvm
-repo init -u git://github.com/AOSiP/platform_manifest.git -b pie -m crowdin.xml --no-tags --current-branch
-time repo sync -j"$(nproc)" --force-sync --force-broken --no-tags --current-branch
-repo forall -j"$(nproc)" -c "scp -p -P 29418 review.aosip.dev:hooks/commit-msg .git/hooks/"
-./crowdin/sync.py "${@}"
