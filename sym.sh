066599c9ca1b70fb2b5b0e6d42ae420d166aca1a
LeCmnGend <namhoang1235@gmail.com>
2022-01-26 10:22:52 +0700

    Clean code

---
+++ sym.sh
@@ -0,0 +1,12 @@
+sudo ln -s /usr/local/bin/ccache /usr/bin/ccache
+sudo ln -s ccache /usr/local/bin/clang
+sudo ln -s ccache /usr/local/bin/clang++
+sudo ln -s ccache /usr/local/bin/cc
+sudo ln -s ccache /usr/local/bin/c++
+sudo ln -s ccache /usr/local/bin/gcc
+sudo ln -s ccache /usr/local/bin/g++
+
+sudo ln -s /usr/bin/python3 /usr/bin/python		
+sudo ln -s /usr/bin/python3 /usr/bin/python2
+sudo ln -s /usr/lib32/libstdc++.so.6 /usr/lib32/libstdc++.so				
+sudo ln -s /usr/lib32/libz.so.1 /usr/lib32/libz.so
