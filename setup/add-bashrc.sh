7b5c942cf50e637b967eb52fd80c08500e4492e7
lecmngend <namhoang1235@gmail.com>
2022-01-26 12:33:20 +0700

    Add /root/tc/proton/clang-13/bin:/usr/lib/jvm/java-11-openjdk-amd64/bin:/root/Android/Sdk/cmdline-tools/latest/bin:/root/Android/Sdk/cmdline-tools/tools/bin:/root/bin:/root/usr/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Program Files/WindowsApps/CanonicalGroupLimited.UbuntuonWindows_2004.2021.825.0_x64__79rhkp1fndgsc:/mnt/c/ProgramData/Oracle/Java/javapath:/mnt/c/Windows/system32:/mnt/c/Windows:/mnt/c/Windows/System32/Wbem:/mnt/c/Windows/System32/WindowsPowerShell/v1.0/:/mnt/c/Windows/System32/OpenSSH/:/mnt/c/ProgramData/chocolatey/bin:/mnt/c/tools/lxrunoffline:/mnt/c/Users/LeCmnGend/AppData/Local/Microsoft/WindowsApps:/snap/bin

--- setup/add-bashrc.sh
+++ setup/add-bashrc.sh
@@ -1,6 +1,6 @@
 echo -e "
 
-export PATH=~/bin:~/usr/bin:/usr/local/bin
+export PATH=~/bin:~/usr/bin:/usr/local/bin:$PATH
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/local/bin/ccache
 export CCACHE_MAXSIZE=50G
@@ -17,10 +17,10 @@ export CCACHE_CC=clang
 export CCACHE_CXX=clang++
 export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime,file_stat_matches
 export ANDROID_SDK_ROOT=~/Android/Sdk
-export PATH=$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$ANDROID_SDK_ROOT/cmdline-tools/tools/bin
+export PATH=$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$ANDROID_SDK_ROOT/cmdline-tools/tools/bin:$PATH
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export PATH=$JAVA_HOME/bin
-export PATH=$HOME/tc/proton/clang-13/bin
+export PATH=$HOME/tc/proton/clang-13/bin:$PATH
 export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF+7 -XX:+TieredCompilation -Xmx8G"
 
 " >> ~/.bashrc
