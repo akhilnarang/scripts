73851179ce3f4c349f47f6781da4ebc701188401
Akhil Narang <me@akhilnarang.dev>
2022-06-27 17:32:47 +0530

    Add in a comment, add in a missing command from github's install docs
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -45,8 +45,9 @@ sudo DEBIAN_FRONTEND=noninteractive \
     libxml-simple-perl libswitch-perl apt-utils \
     ${PACKAGES} -y
 
-echo -e "Install GitHub CLI"
+echo -e "Installing GitHub CLI"
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
 sudo apt install -y gh
