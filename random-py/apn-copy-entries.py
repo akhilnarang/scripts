10860bac21b99401fe050b6e572854d9e8950d9a
Omkar Chandorkar <gotenksIN@aospa.co>
2024-04-07 00:02:27 +0530

    random: Add scripts to copy IMS APN as well as sort APNs
    (cherry picked from commit c6201a816f3029e00376aa93b40ebafee68e1190)
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- random-py/apn-copy-entries.py
+++ random-py/apn-copy-entries.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 from xml.etree import ElementTree
 
-# ./apn.py source.xml destination.xml
+# ./apn-copy-entries.py source.xml destination.xml
 # If no arguments are provided, the user will be prompted to enter the file paths
 
 if len(sys.argv) < 3:
@@ -25,19 +25,25 @@ source_apn_xml = ElementTree.parse(source_path)
 destination_apn_xml = ElementTree.parse(destination_path)
 
 tags_to_match = ("mcc", "mnc")
-tags_to_copy = ("apn",)
-required_tags = tags_to_match + tags_to_copy
+tags_to_copy = ("apn")
 
 destination_root = destination_apn_xml.getroot()
 
 combinations = set()
 
-for child in destination_root:
-    combinations.add("|".join([child.attrib[tag] for tag in tags_to_match]))
+# Define a function to check if a child element is already in the destination root
+def is_in_destination(child, destination_root):
+    for dest_child in destination_root:
+        if child.attrib == dest_child.attrib:
+            return True
+    return False
 
+# Modify the loop that appends children from the source to the destination
 for child in source_apn_xml.getroot():
-    key = "|".join([child.attrib[tag] for tag in tags_to_match])
-    if key in combinations:
-        destination_root.append(child)
+    # Only consider children with apn='ims'
+    if child.attrib.get('apn') == 'ims':
+        # Only append the child if it is not already in the destination
+        if not is_in_destination(child, destination_root):
+            destination_root.append(child)
 
 destination_apn_xml.write(destination, encoding="utf-8")
